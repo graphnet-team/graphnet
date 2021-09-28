9b33991a33d3a3d6c6c2de9d2ee3bceec261ec72
Andreas Søgaard <andreas.sogaard@gmail.com>
2021-09-28 16:58:08 +0200

    Remove dummy file

--- scratchpad.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class _SUPPORTED_MODES:
-    inference = 'inference'
-    oscNext = 'oscNext'
-    NuGen = 'NuGen'
-
-print([e for e in _SUPPORTED_MODES])
-print(_SUPPORTED_MODES.inference == 'inference')
