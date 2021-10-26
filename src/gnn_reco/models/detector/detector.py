cc78195f784effc3460e02147f9b02e1f4262b2b
Andreas Søgaard <andreas.sogaard@gmail.com>
2021-10-26 08:59:19 +0200

    Move common functionality to base Detector class

--- src/gnn_reco/models/detector/detector.py
+++ src/gnn_reco/models/detector/detector.py
@@ -1,7 +1,8 @@
 from abc import abstractmethod
-from typing import List
+from typing import List, final
 
 from torch.nn import Module
+from torch_geometric.data import Data
 
 from gnn_reco.models.graph_builders import GraphBuilder
 
@@ -21,11 +22,29 @@ class Detector(Module):
         # Member variables
         self._graph_builder = graph_builder
 
+    @final
+    def forward(self, data: Data) -> Data:
+        # Convenience variables
+        assert data.x.size()[1] == self.nb_inputs, f"Got graph data with incompatible size, {data.x.size()} vs. {self.nb_inputs} expected"
+        
+        # Graph-bulding
+        data = self._graph_builder(data).clone()  # `.clone` is necessary to avoid modifying original tensor in-place
+
+        # Implementation-specific forward pass (e.g. preprocessing)
+        data = self._forward(data)
+
+        return data
+
+    @abstractmethod
+    def _forward(self, data: Data) -> Data:
+        """Same syntax as `.forward` for implentation in inheriting classes."""
+        pass
+
     @property
-    def nb_inputs(self):
+    def nb_inputs(self) -> int:
         return len(self.features)
 
     @property
-    def nb_outputs(self):
+    def nb_outputs(self) -> int:
         """This the default, but may be overridden by specific inheriting classes."""
         return self.nb_inputs
