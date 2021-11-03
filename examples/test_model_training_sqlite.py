e7bccfacdc43a67069cff2d8884923c8dc3ca81f
Andreas Søgaard <andreas.sogaard@gmail.com>
2021-11-03 12:59:34 +0100

    Add working example of training with scalers and legacy von-Mises Fisher loss

--- examples/test_model_training_sqlite.py
+++ examples/test_model_training_sqlite.py
@@ -2,14 +2,15 @@ from timer import timer
 import logging
 
 import torch
-from gnn_reco.data.sqlite_dataset import SQLiteDataset
+#from gnn_reco.data.sqlite_dataset import SQLiteDataset
 
 from gnn_reco.models.detector.icecube86 import IceCube86
 from gnn_reco.models.graph_builders import KNNGraphBuilder
 from gnn_reco.models import Model
-from gnn_reco.models.task.reconstruction import AngularReconstruction, EnergyReconstruction
+from gnn_reco.models.task.reconstruction import AngularReconstructionWithKappa, EnergyReconstruction, AngularReconstruction, LegacyAngularReconstruction
 
-from gnn_reco.components.loss_functions import LogCosh, LogCoshOfLogTransformed
+from gnn_reco.components.loss_functions import LogCoshLoss, VonMisesFisher2DLoss, LegacyVonMisesFisherLoss
+from gnn_reco.components.utils import fit_scaler
 from gnn_reco.models.training.trainers import Trainer, Predictor
 from gnn_reco.models.training.callbacks import PiecewiseLinearScheduler
 from gnn_reco.models.training.utils import make_train_validation_dataloader, save_results
@@ -38,11 +39,14 @@ def main():
     batch_size = 1024
     num_workers = 10
     device = 'cuda:1'
-    target = 'energy'
+    target = 'zenith'
     n_epochs = 30
     patience = 5
     archive = '/groups/icecube/asogaard/gnn/results'
-    
+
+    # Scalers
+    scalers = fit_scaler(db, features, truth, pulsemap)
+
     # Common variables
     selection = get_even_neutrino_indicies(db)[0:100000]
 
@@ -59,16 +63,22 @@ def main():
     # Building model
     detector = IceCube86(
         graph_builder=KNNGraphBuilder(nb_nearest_neighbours=8),
+        scalers=scalers['input']['SRTTWOfflinePulsesDC'],
     )
     gnn = DynEdge(
         nb_inputs=detector.nb_outputs,
         nb_outputs=16,
     )
-    task = EnergyReconstruction(
+    task = LegacyAngularReconstruction(
         hidden_size=gnn.nb_outputs, 
         target_label=target, 
-        loss_function=LogCoshOfLogTransformed(),
+        loss_function=LegacyVonMisesFisherLoss(),
     )
+    #task = AngularReconstructionWithKappa(
+    #    hidden_size=gnn.nb_outputs, 
+    #    target_label=target, 
+    #    loss_function=VonMisesFisher2DLoss(),
+    #)
     model = Model(
         detector=detector,
         gnn=gnn,
@@ -76,13 +86,14 @@ def main():
         device=device,
     )
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5, eps=1e-03)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-8, eps=1e-03)
     scheduler = PiecewiseLinearScheduler(training_dataloader, start_lr=1e-5, max_lr=1e-3, end_lr=1e-5, max_epochs=n_epochs)
-    
+
     trainer = Trainer(
         training_dataloader=training_dataloader,
         validation_dataloader=validation_dataloader, 
-        optimizer=optimizer, n_epochs=n_epochs, 
+        optimizer=optimizer,
+        n_epochs=n_epochs, 
         scheduler=scheduler,
         patience=patience,
     )
@@ -92,8 +103,15 @@ def main():
     except KeyboardInterrupt:
         print("[ctrl+c] Exiting gracefully.")
         pass
-    
-    predictor = Predictor(dataloader=validation_dataloader, target=target, device=device, output_column_names=[target + '_pred', target + '_uncert'])
+
+
+    predictor = Predictor(
+        dataloader=validation_dataloader, 
+        target=target, 
+        device=device, 
+        output_column_names=[target + '_pred', target + '_sigma'],
+    )
+    trained_model._tasks[0].inference = True
     results = predictor(trained_model)
     save_results(db, 'dynedge_energy', results,archive, trained_model)
 
