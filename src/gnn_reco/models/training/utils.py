501d6aae43204ea79a97cf4d842c27c52804b55e
Andreas Søgaard <andreas.sogaard@gmail.com>
2021-11-16 09:29:13 +0100

    Resolve duplicate make_train_validation_dataloader function

--- src/gnn_reco/models/training/utils.py
+++ src/gnn_reco/models/training/utils.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import dill
 import numpy as np
@@ -14,14 +14,14 @@ from gnn_reco.models import Model
 
 def make_dataloader(
     db: str,
-    selection: Optional[str],
     pulsemap: str,
-    batch_size: int,
     features: List[str],
     truth: List[str],
+    batch_size: int,
     shuffle: bool,
-    num_workers: int,
-    persistent_workers: bool =True,
+    selection: List[int] = None,
+    num_workers: int = 10,
+    persistent_workers: bool = True,
 ) -> DataLoader:
     
     dataset = SQLiteDataset(
@@ -44,25 +44,57 @@ def make_dataloader(
 
     return dataloader
 
-def make_train_validation_dataloader(db, selection, pulsemap, batch_size, features, truth, num_workers, persistent_workers=True):
-    training_selection, validation_selection = train_test_split(selection, test_size=0.33, random_state=42)
+def make_train_validation_dataloader(
+    db: str,
+    selection: List[int],
+    pulsemap: str,
+    features: List[str],
+    truth: List[str],
+    batch_size: int,
+    database_indices: List[int] = None,
+    seed: int = 42,
+    test_size: float = 0.33,
+    num_workers: int = 10,
+    persistent_workers: bool = True,
+) -> Tuple[DataLoader]:
 
+    # Reproducibility
+    rng = np.random.RandomState(seed=seed)
+
+    # Perform train/validation split
+    if isinstance(db, list):
+        df_for_shuffle = pd.DataFrame({'event_no': selection, 'db': database_indices})
+        shuffled_df = df_for_shuffle.sample(frac=1, replace=False, random_state=rng)
+        training_df, validation_df = train_test_split(shuffled_df, test_size=test_size, random_state=rng)
+        training_selection = training_df.values.tolist()
+        validation_selection = validation_df.values.tolist()
+    else:
+        training_selection, validation_selection = train_test_split(selection, test_size=test_size, random_state=rng)
+
+    # Create DataLoaders
     common_kwargs = dict(
+        db=db,
+        pulsemap=pulsemap,
+        features=features,
+        truth=truth,
         batch_size=batch_size,
-        shuffle=True,
         num_workers=num_workers,
-        collate_fn=Batch.from_data_list,
         persistent_workers=persistent_workers,
-        prefetch_factor=2,
+    )
+
+    training_dataloader = make_dataloader(
+        shuffle=True,
+        selection=training_selection,
+        **common_kwargs,
     )
     
-    training_dataset = SQLiteDataset(db, pulsemap, features, truth, selection=training_selection)
-    training_dataloader = torch.utils.data.DataLoader(training_dataset, **common_kwargs)
-
-    validation_dataset = SQLiteDataset(db, pulsemap, features, truth, selection=validation_selection)
-    validation_dataloader = torch.utils.data.DataLoader(validation_dataset, **common_kwargs)
-
-    return training_dataloader, validation_dataloader
+    validation_dataloader = make_dataloader(
+        shuffle=False,
+        selection=validation_selection,
+        **common_kwargs,
+    )
+    
+    return training_dataloader, validation_dataloader  # , {'valid_selection':validation_selection, 'training_selection':training_selection}
 
 def save_results(db, tag, results, archive,model):
     db_name = db.split('/')[-1].split('.')[0]
