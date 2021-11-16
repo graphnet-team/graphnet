86b901d1ba4dc6e8cc50201befa881241b875378
Andreas Søgaard <andreas.sogaard@gmail.com>
2021-11-16 10:57:24 +0100

    Add separate zenith and azimuth reconstruction tasks

--- src/gnn_reco/models/task/reconstruction.py
+++ src/gnn_reco/models/task/reconstruction.py
@@ -1,36 +1,53 @@
+import numpy as np
 import torch
 
 from gnn_reco.components.loss_functions import LossFunction
 from gnn_reco.models.task import Task
 from gnn_reco.utils import eps_like
 
-class AngularReconstruction(Task):
+class AzimuthReconstruction(Task):
     # Requires two features: untransformed points in (x,y)-space.
     nb_inputs = 2
 
     def _forward(self, x):
         # Transform outputs to angle and prepare prediction
-        return torch.atan2(x[:,1], x[:,0]).unsqueeze(1)  # atan(y,x)
+        radius = torch.sqrt(x[:,0]**2 + x[:,1]**2)
+        beta = 0.01
+        kl_loss = torch.mean(radius**2 - torch.log(radius) - 1)
+        self._regularisation_loss += beta * kl_loss
+        return torch.atan2(x[:,1], x[:,0]).unsqueeze(1) + np.pi  # atan(y,x) -> [-pi, pi]
+        
+        #return torch.sigmoid(x[:,:1]) * 2 * np.pi
 
-class AngularReconstructionWithKappa(AngularReconstruction):
-    # Requires one feature in addition to `AngularReconstruction`: kappa (unceratinty; 1/variance).
+class ZenithReconstruction(Task):
+    # Requires two features: untransformed points in (x,y)-space.
+    nb_inputs = 1
+
+    def _forward(self, x):
+        # Transform outputs to angle and prepare prediction
+        return torch.sigmoid(x[:,:1]) * np.pi
+
+
+class AzimuthReconstructionWithKappa(AzimuthReconstruction):
+    # Requires one feature in addition to `AzimuthReconstruction`: kappa (unceratinty; 1/variance).
     nb_inputs = 3
 
     def _forward(self, x):
         # Transform outputs to angle and prepare prediction
         angle = super()._forward(x[:,:2]).squeeze(1)
-        kappa = torch.nn.functional.softplus(x[:,2]) + eps_like(x)
+        kappa = torch.abs(x[:,2]) + eps_like(x)
         return torch.stack((angle, kappa), dim=1)
 
-class AngularReconstructionWithUncertainty(AngularReconstruction):
-    # Requires one feature in addition to `AngularReconstruction`: log-variance (uncertainty).
-    nb_inputs = 3
+class ZenithReconstructionWithKappa(ZenithReconstruction):
+    # Requires one feature in addition to `ZenithReconstruction`: kappa (unceratinty; 1/variance).
+    nb_inputs = 2
 
     def _forward(self, x):
         # Transform outputs to angle and prepare prediction
-        angle = super()._forward(x[:,:2]).squeeze(1)
-        log_var = x[:,2]
-        return torch.stack((angle, log_var), dim=1)
+        angle = super()._forward(x[:,:1]).squeeze(1)
+        kappa = torch.abs(x[:,1]) + eps_like(x)
+        return torch.stack((angle, kappa), dim=1)
+        
 
 class EnergyReconstruction(Task):
     # Requires one feature: untransformed energy
