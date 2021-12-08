8a3e09e2b091952a998010696c776381de55972f
Kaare Endrup Iversen <kaare@hep03.cluster>
2021-12-08 12:45:07 +0100

    Added interaction time to constants

--- src/gnn_reco/data/constants.py
+++ src/gnn_reco/data/constants.py
@@ -34,6 +34,7 @@ class TRUTH:
         'elasticity', 
         'sim_type', 
         'interaction_type',
+        'interaction_time',                               ## Added for vertex reconstruction
     ]
     DEEPCORE = ICECUBE86
     UPGRADE = DEEPCORE
