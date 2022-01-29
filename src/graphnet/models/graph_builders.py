e4fe5bf3334014e1f5d8e4870d9ca3fdc73b5fd1
bozianuleon <qzr746@alumni.ku.dk>
2022-01-29 19:03:43 +0100

    Radial graph builder from torch_geometric

--- src/graphnet/models/graph_builders.py
+++ src/graphnet/models/graph_builders.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from torch_geometric.nn import knn_graph
+from torch_geometric.nn import knn_graph,radius_graph
 from torch_geometric.data import Data
 
 
@@ -37,6 +37,38 @@ class KNNGraphBuilder(GraphBuilder):  # pylint: disable=too-few-public-methods
         data.edge_index = knn_graph(
             data.x[:, self._columns],
             self._nb_nearest_neighbours,
+            data.batch,
+        ).to(self._device)
+
+        return data
+
+
+class RadialGraphBuilder(GraphBuilder):  
+    """Builds graph adjacency according to a sphere of chosen radius centred at each DOM hit"""
+    def __init__ (
+        self,
+        radius: float,
+        columns: List[int] = None,
+        device: str = None,
+    ):
+        # Check(s)
+        if columns is None:
+            columns = [0,1,2]
+
+        # Member variable(s)
+        self._radius = radius
+        self._columns = columns
+        self._device = device
+
+    def __call__ (self, data: Data) -> Data:
+        # Constructs the adjacency matrix from the raw, DOM-level data and returns this matrix
+        if data.edge_index is not None:
+            print("WARNING: GraphBuilder received graph with pre-existing structure. ",
+                  "Will overwrite.")
+
+        data.edge_index = radius_graph(
+            data.x[:, self._columns],
+            self._radius,
             data.batch,
         ).to(self._device)
 
