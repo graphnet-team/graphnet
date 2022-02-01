0abd70ce89768a018d0e82b457e65496cdf0aab5
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-02-01 10:48:10 +0100

    Cleanup pool.py module, and add a few docstrings

--- src/graphnet/components/pool.py
+++ src/graphnet/components/pool.py
@@ -17,22 +17,37 @@ def sum_pool_and_distribute(tensor: Tensor, cluster_index: LongTensor, batch: Op
     tensor_unpooled = tensor_pooled[inv]
     return tensor_unpooled
 
+
 def group_identical(tensor: Tensor, batch: Optional[LongTensor] = None) -> Tensor:
+    """Group rows in `tensor` that are identical
+
+    Args:
+        tensor (Tensor): Tensor of shape [N, F]
+        batch (Optional[LongTensor], optional): Batch indices, to only group
+            identical rows within batches. Defaults to None.
+
+    Returns:
+        Tensor: List of group indices, from 0 to num. groups - 1, assigning all
+            identical rows to the same group.
+    """
     if batch is not None:
         tensor = tensor.cat((tensor, batch.unsqueeze(dim=1)), dim=1)
     return torch.unique(tensor, return_inverse=True, dim=0)[1]
 
 def group_pulses_to_dom(data: Data) -> Data:
+    """Groups pulses on the same DOM, using DOM and string number."""
     tensor = torch.stack((data.dom_number, data.string)).T.int()
     batch = getattr(tensor, 'batch', None)
     data.dom_index = group_identical(tensor, batch)
     return data
 
 def group_pulses_to_pmt(data: Data) -> Data:
+    """Groups pulses on the same PMT, using PMT, DOM, and string number."""
     tensor = torch.stack((data.pmt_number, data.dom_number, data.string)).T.int()
     batch = getattr(tensor, 'batch', None)
     data.pmt_index = group_identical(tensor, batch)
     return data
+
 
 # Below mirroring `torch_geometric.nn.pool.{avg,max}_pool.py` exactly
 def _sum_pool_x(cluster, x, size: Optional[int] = None):
@@ -101,77 +116,5 @@ def sum_pool(cluster, data, transform=None):
 
     if transform is not None:
         data = transform(data)
-
-    return data
-
-#############################################################
-
-def cluster_identical(tensor: Tensor, batch: Optional[LongTensor] = None, eps: float = 1e-03) -> Tensor:
-    """Cluster identical rows in `tensor`.
-
-    Args:
-        tensor (Tensor): 2D tensor.
-        eps (float, optional): Radius-style threshold for clustering rows in
-            `tensor`. Defaults to 1e-03.
-
-    Returns:
-        Tensor: 1D array of integers assigning each row in `tensor` to a cluter.
-    """
-    # Check(s)
-    assert tensor.ndim == 2
-
-    # By default assign each pulse to a separate DOM
-    cluster_index = torch.arange(tensor.size(dim=0), device=tensor.device)
-
-    if batch is not None:
-        for ix_batch in torch.unique(batch):
-            cluster_index[batch == ix_batch] = cluster_identical(tensor[batch == ix_batch])
-        return cluster_index
-
-    # Get pairs
-    nb_samples = tensor.size(dim=0)
-    mask_triu = torch.ones(nb_samples, nb_samples).triu(1).bool()
-    dist = torch.triu((tensor.T.unsqueeze(dim=0) - tensor.unsqueeze(dim=2)).abs().sum(dim=1))
-    pairs = torch.stack(torch.where((dist < eps) & mask_triu)).T
-
-    # Connect all pulses within a radius of < eps, and assign them to the same DOM
-    for pair in pairs:
-        cluster_index[pair[1]] = cluster_index[pair[0]]
-
-    # Ensure that DOMs are squentially numbered from 0 to nb_clusters - 1
-    ix_sort = torch.argsort(cluster_index)
-    cluster_index_sorted = torch.cumsum(torch.diff(cluster_index[ix_sort], prepend=torch.tensor([0])).clip(0, 1), dim=0)
-    cluster_index[ix_sort] = cluster_index_sorted
-
-    return cluster_index
-
-def cluster_pulses_to_dom(data: Data, eps: float = 1e-03) -> Data:
-    # Could also use `dom_number` and `string` -- but these only exist for
-    # IceCubeUpgrade, not for <= DeepCore
-
-    # Extract spatial coordinates for each pulse
-    xyz = torch.stack((data['dom_x'], data['dom_y'], data['dom_z']), dim=1)
-
-    # Add DOM clustering as an attribute to the `Data` object
-    data.dom_index = cluster_identical(xyz, getattr(data, 'batch', None))
-
-    return data
-
-def cluster_pulses_to_pmt(data: Data, eps: float = 1e-03) -> Data:
-    # Could also use `pmt_number`, `dom_number`, and `string` -- but these only
-    # exist for IceCubeUpgrade, not for <= DeepCore
-
-    # Extract spatial coordinates for each pulse
-    xyzdir = torch.stack((
-            data['dom_x'],
-            data['dom_y'],
-            data['dom_z'],
-            data['pmt_dir_x'],
-            data['pmt_dir_y'],
-            data['pmt_dir_z'],
-        ), dim=1)
-
-    # Add pmt clustering as an attribute to the `Data` object
-    data.pmt_index = cluster_identical(xyzdir, getattr(data, 'batch', None))
 
     return data
