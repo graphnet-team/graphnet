75fbc8e94a8714cb4543a6899c92671f507a3ff9
Rasmus Oersoe <rahn@outlook.dk>
2022-02-02 18:52:01 +0100

    removed redundant Predictor Class

--- src/graphnet/models/training/utils.py
+++ src/graphnet/models/training/utils.py
@@ -167,39 +167,3 @@ def load_model(db, tag, archive, detector, gnn, task, device):
     model.load_state_dict(torch.load(path + '/' + tag + '.pth'))
     model.eval()
     return model
-
-class Predictor(object):
-    def __init__(self, dataloader, target, device, output_column_names, post_processing_method = None):
-        self.dataloader = dataloader
-        self.target = target
-        self.output_column_names = output_column_names
-        self.device = device
-        self.post_processing_method = post_processing_method
-
-    def __call__(self, model):
-        self.model = model
-        self.model.eval()
-        self.model.predict = True
-        if self.post_processing_method == None:
-            return self._predict()
-        else:
-            return self.post_processing_method(self._predict(), self.target)
-
-    def _predict(self):
-        assert len(self.model._tasks) == 1
-        predictions = []
-        event_nos = []
-        energies = []
-        target = []
-        with torch.no_grad():
-            for batch_of_graphs in tqdm(self.dataloader, unit = 'batches'):
-                batch_of_graphs.to(self.device)
-                target.extend(batch_of_graphs[self.target].detach().cpu().numpy())
-                predictions.extend(self.model(batch_of_graphs)[0].detach().cpu().numpy())
-                event_nos.extend(batch_of_graphs['event_no'].detach().cpu().numpy())
-                energies.extend(batch_of_graphs['energy'].detach().cpu().numpy())
-        out = pd.DataFrame(data = predictions, columns = self.output_column_names)
-        out['event_no'] = event_nos
-        out['energy'] = energies
-        out[self.target] = target
-        return out
