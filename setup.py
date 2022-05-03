91f947e36f77e0fd4404cb2a5db2c09032316d41
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-05-03 10:24:08 +0200

    Add pre-commit to setup.py

--- setup.py
+++ setup.py
@@ -3,65 +3,73 @@ import sys
 from setuptools import setup, find_packages
 import versioneer
 
+
 # Utility method(s)
 def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", package])
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--user", package]
+    )
+
 
 # Requirements definitions
 SETUP_REQUIRES = [
-    'setuptools >= 58.2.0',
+    "setuptools >= 58.2.0",
 ]
 
 INSTALL_REQUIRES = [
-    'sqlalchemy',
-    'pandas',
-    'numpy',
-    'timer',
-    'tqdm',
-    'torch-cluster==1.5.9',
-    'torch-scatter==2.0.9',
-    'torch-sparse==0.6.12',
-    'torch-spline-conv==1.2.1',
-    'torch-geometric==2.0.1',
-    'pytorch-lightning==1.5.6',
-    'dill',
-    'wandb',
-    'matplotlib',
+    "sqlalchemy",
+    "pandas",
+    "numpy",
+    "timer",
+    "tqdm",
+    "torch-cluster==1.5.9",
+    "torch-scatter==2.0.9",
+    "torch-sparse==0.6.12",
+    "torch-spline-conv==1.2.1",
+    "torch-geometric==2.0.1",
+    "pytorch-lightning==1.5.6",
+    "dill",
+    "wandb",
+    "matplotlib",
 ]
 
 EXTRAS_REQUIRE = {
-    'develop': [
-        'pytest',
-        'pylint',
-        'pydocstyle',
-        'coverage',
-        'anybadge',
-        'sphinx',
-        'sphinx_rtd_theme',
-        'versioneer',
+    "develop": [
+        "pytest",
+        "pylint",
+        "pydocstyle",
+        "coverage",
+        "anybadge",
+        "pre-commit" "sphinx",
+        "sphinx_rtd_theme",
+        "versioneer",
     ],
 }
 
-# Ensure pytorch is already installed (see e.g. https://github.com/pyg-team/pytorch_geometric/issues/861#issuecomment-566424944)
+# Ensure pytorch is already installed (see e.g.
+# https://github.com/pyg-team/pytorch_geometric/issues/861#issuecomment-566424944)
 try:
-    import torch  # pyright: reportMissingImports=false
+    import torch  # noqa: F401 # pyright: reportMissingImports=false
 except ImportError:
-    install('torch==1.9.0')
+    install("torch==1.9.0")
 
 setup(
-    name='graphnet',
+    name="graphnet",
     version=versioneer.get_version(),
-    description='A common library for using graph neural networks (GNNs) in netrino telescope experiments.',
-    license='Apache 2.0',
-    author='The IceCube Collaboration',
-    url='https://github.com/icecube/graphnet',
+    description=(
+        "A common library for using graph neural networks (GNNs) in netrino "
+        "telescope experiments."
+    ),
+    license="Apache 2.0",
+    author="The IceCube Collaboration",
+    url="https://github.com/icecube/graphnet",
     cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     setup_requires=SETUP_REQUIRES,
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
     dependency_links=[
-        'https://data.pyg.org/whl/torch-1.9.0+cpu.html',
+        "https://data.pyg.org/whl/torch-1.9.0+cpu.html",
     ],
 )
