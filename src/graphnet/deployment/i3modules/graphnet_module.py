7b838080a7070b5934ed665f207e20940a69e042
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-06-21 10:30:04 +0200

    Toggle inference mode

--- src/graphnet/deployment/i3modules/graphnet_module.py
+++ src/graphnet/deployment/i3modules/graphnet_module.py
@@ -100,6 +100,10 @@ class GraphNeTModuleBase(I3Module):
         else:
             self.model = model
 
+        # Toggle inference mode on, to ensure that any transforms of the model
+        # predictions are applied.
+        self.model.inference()
+
     def Physics(self, frame: I3Frame):  # pylint: disable=invalid-name
         """Process Physics I3Frame and write predictions."""
 
