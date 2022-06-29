4bce695a6c3205d8f84dc8212ccd4c26843bfa09
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-06-29 16:32:34 +0200

    Update SQLiteDatasetPerturbed to be consistent with updated SQLiteDataset class

--- src/graphnet/data/sqlite/sqlite_dataset_perturbed.py
+++ src/graphnet/data/sqlite/sqlite_dataset_perturbed.py
@@ -1,5 +1,9 @@
+from typing import Dict, List, Optional, Tuple, Union
+
 import numpy as np
 import torch
+from torch_geometric.data import Data
+
 from graphnet.data.sqlite.sqlite_dataset import SQLiteDataset
 
 
@@ -8,44 +12,58 @@ class SQLiteDatasetPerturbed(SQLiteDataset):
 
     def __init__(
         self,
-        database,
-        pulsemap_table,
-        features,
-        truth,
-        perturbation_dict,
-        index_column="event_no",
-        truth_table="truth",
-        selection=None,
-        dtype=torch.float32,
+        path: Union[str, List[str]],
+        pulsemaps: Union[str, List[str]],
+        features: List[str],
+        truth: List[str],
+        *,
+        perturbation_dict: Dict,
+        node_truth: Optional[List[str]] = None,
+        index_column: str = "event_no",
+        truth_table: str = "truth",
+        node_truth_table: Optional[str] = None,
+        string_selection: Optional[List[int]] = None,
+        selection: Optional[List[int]] = None,
+        dtype: torch.dtype = torch.float32,
     ):
+        # Base class constructor
+        super().__init__(
+            path=path,
+            pulsemaps=pulsemaps,
+            features=features,
+            truth=truth,
+            node_truth=node_truth,
+            index_column=index_column,
+            truth_table=truth_table,
+            node_truth_table=node_truth_table,
+            string_selection=string_selection,
+            selection=selection,
+            dtype=dtype,
+        )
 
+        # Custom member variables
         assert isinstance(perturbation_dict, dict)
         assert len(set(perturbation_dict.keys())) == len(
             perturbation_dict.keys()
         )
         self._perturbation_dict = perturbation_dict
-        super().__init__(
-            database,
-            pulsemap_table,
-            features,
-            truth,
-            index_column,
-            truth_table,
-            selection,
-            dtype,
-        )
+
         self._perturbation_cols = [
             self._features.index(key) for key in self._perturbation_dict.keys()
         ]
 
-    def __getitem__(self, i):
-        self._establish_connection(i)
-        features, truth = self._query_database(i)
+    def __getitem__(self, index: int) -> Data:
+        assert (
+            0 <= index < len(self)
+        ), f"Index {index} not in range [0, {len(self) - 1}]"
+        features, truth, node_truth = self._query(index)
         perturbed_features = self._perturb_features(features)
-        graph = self._create_graph(perturbed_features, truth)
+        graph = self._create_graph(perturbed_features, truth, node_truth)
         return graph
 
-    def _perturb_features(self, features):
+    def _perturb_features(
+        self, features: List[Tuple[float]]
+    ) -> List[Tuple[float]]:
         features = np.array(features)
         perturbed_features = np.random.normal(
             loc=features[:, self._perturbation_cols],
@@ -54,4 +72,4 @@ class SQLiteDatasetPerturbed(SQLiteDataset):
             ),
         )
         features[:, self._perturbation_cols] = perturbed_features
-        return features
+        return features.tolist()
