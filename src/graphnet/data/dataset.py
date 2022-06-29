f3d40e829b3c44ac4477f4cce666a756e39d41fc
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-06-29 11:33:03 +0200

    Update Dataset class to use node truth features, selection, and to add labels to output graph

--- src/graphnet/data/dataset.py
+++ src/graphnet/data/dataset.py
@@ -9,15 +9,20 @@ from torch_geometric.data import Data
 from graphnet.utilities.logging import LoggerMixin
 
 
+class ColumnMissingException(Exception):
+    """Exception to indicate a missing column in a dataset."""
+
+
 class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
     """Base Dataset class for reading from any intermediate file format."""
 
     def __init__(
         self,
-        path: str,
+        path: Union[str, List[str]],
         pulsemaps: Union[str, List[str]],
         features: List[str],
         truth: List[str],
+        *,
         node_truth: Optional[List[str]] = None,
         index_column: str = "event_no",
         truth_table: str = "truth",
@@ -27,53 +32,65 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
         dtype: torch.dtype = torch.float32,
     ):
         # Check(s)
-        if isinstance(path, list):
-            self.logger.error("Multiple folders not supported")
-        assert isinstance(path, str)
-
         if isinstance(pulsemaps, str):
             pulsemaps = [pulsemaps]
 
         assert isinstance(features, (list, tuple))
         assert isinstance(truth, (list, tuple))
 
-        assert (
-            node_truth is None
-        ), "Argument `node_truth` is currently not supported."
-        assert (
-            node_truth_table is None
-        ), "Argument `node_truth_table` is currently not supported."
-
-        assert (
-            string_selection is None
-        ), "Argument `string_selection` is currently not supported"
-
         # Member variable(s)
         self._path = path
         self._selection = None
         self._pulsemaps = pulsemaps
-        self._features = features
-        self._truth = truth
+        self._features = [index_column] + features
+        self._truth = [index_column] + truth
         self._index_column = index_column
         self._truth_table = truth_table
+
+        if node_truth is not None:
+            assert isinstance(node_truth_table, str)
+            if isinstance(node_truth, str):
+                node_truth = [node_truth]
+
+        self._node_truth = node_truth
+        self._node_truth_table = node_truth_table
+
+        if string_selection is not None:
+            self.logger.warning(
+                (
+                    "String selection detected.\n",
+                    f"Accepted strings: {string_selection}\n",
+                    "All other strings are ignored!",
+                )
+            )
+            if isinstance(string_selection, int):
+                string_selection = [string_selection]
+
+        self._string_selection = string_selection
+
+        self._selection = None
+        if self._string_selection:
+            self._selection = f"string in {str(tuple(self._string_selection))}"
+
         self._dtype = dtype
 
         # Implementation specific initialisation.
         self._initialise()
 
-        # Purely internal member variables
-        self._missing_variables = dict()
-        self._remove_missing_columns()
-
+        # Set unique indices
         if selection is None:
             self._indices = self._get_all_indices()
         else:
             self._indices = selection
 
+        # Purely internal member variables
+        self._missing_variables = dict()
+        self._remove_missing_columns()
+
     # Abstract method(s)
     @abstractmethod
-    def _set_input_file(self, path: str):
-        """Set any internal representation needed to read from `path`."""
+    def _initialise(self):
+        """Set any internal representation needed to read data from input file."""
 
     @abstractmethod
     def _get_all_indices(self) -> List[int]:
@@ -83,10 +100,10 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
     def _query_table(
         self,
         table: str,
-        columns: List[str],
+        columns: Union[List[str], str],
         index: int,
         selection: Optional[str] = None,
-    ) -> Union[List[Tuple[Any]], Tuple[Any]]:
+    ) -> List[Tuple[Any]]:
         """Query a table at a specific index, optionally subject to some selection.
 
         Args:
@@ -99,17 +116,24 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
                 reading out data. Defaults to None.
 
         Returns:
-            Union[List[Tuple[Any]], Tuple[Any]]:  Returns a list of tuples if
-                the `table` contains array-type data; otherwise returns a single
-                tuple of `table` contains only scalar data.
+            List[Tuple[Any]]: Returns a list of tuples containing the values in
+                `columns`. If the `table` contains only scalar data for `columns`,
+                a list of length 1 is returned
+
+        Raises:
+            ColumnMissingException: If one or more element in `columns` is not
+                present in `table`.
         """
 
     # Public method(s)
     def __len__(self):
         return len(self._indices)
 
-    def __getitem__(self, i: int) -> Data:
-        features, truth, node_truth = self._query(i)
+    def __getitem__(self, index: int) -> Data:
+        assert index >= 0 and index < len(
+            self
+        ), f"Index {index} not in range [0, {len(self) - 1}]"
+        features, truth, node_truth = self._query(index)
         graph = self._create_graph(features, truth, node_truth)
         return graph
 
@@ -157,14 +181,16 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
         for column in columns:
             try:
                 self._query_table(table, [column], 0)
-            except ValueError:
+            except ColumnMissingException:
                 if table not in self._missing_variables:
                     self._missing_variables[table] = []
                 self._missing_variables[table].append(column)
 
         return self._missing_variables.get(table, [])
 
-    def _query(self, index: int) -> Tuple[List[Tuple], Tuple, List[Tuple]]:
+    def _query(
+        self, index: int
+    ) -> Tuple[List[Tuple], List[Tuple], List[Tuple]]:
         """Query file for event features and truth information
 
         Args:
@@ -174,7 +200,7 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
 
         Returns:
             List[Tuple]: Pulse-level event features.
-            Tuple: Event-level truth information.
+            List[Tuple]: Event-level truth information. List has length 1.
             List[Tuple]: Pulse-level truth information.
         """
 
@@ -186,10 +212,23 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
             features.extend(features_pulsemap)
 
         truth = self._query_table(self._truth_table, self._truth, index)
-        node_truth = None
+        if self._node_truth:
+            node_truth = self._query_table(
+                self._node_truth_table,
+                self._node_truth,
+                index,
+                self._selection,
+            )
+        else:
+            node_truth = None
         return features, truth, node_truth
 
-    def _create_graph(self, features, truth, node_truth=None):
+    def _create_graph(
+        self,
+        features: List[Tuple[Any]],
+        truth: List[Tuple[Any]],
+        node_truth: Optional[List[Tuple[Any]]] = None,
+    ) -> Data:
         """Create Pytorch Data (i.e.graph) object.
 
         No preprocessing is performed at this stage, just as no node adjancency
@@ -205,9 +244,11 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
             torch.Data: Graph object.
         """
         # Convert nested list to simple dict
-        truth_dict = {key: truth[ix] for ix, key in enumerate(self._truth)}
+        truth_dict = {key: truth[0][ix] for ix, key in enumerate(self._truth)}
+        assert len(truth) == 1
 
-        # assert len(truth) == len(self._truth)
+        # Define custom labels
+        labels_dict = self._get_labels(truth_dict)
 
         # Convert nested list to simple dict
         if node_truth is not None:
@@ -217,15 +258,21 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
                 for ix, key in enumerate(self._node_truth)
             }
 
+        # Catch cases with no reconstructed pulses
+        if len(features):
+            data = np.asarray(features)[:, 1:]
+        else:
+            data = np.array([]).reshape((0, len(self._features) - 1))
+
         # Construct graph data object
-        x = torch.tensor(features, dtype=self._dtype)
+        x = torch.tensor(data, dtype=self._dtype)
         n_pulses = torch.tensor(len(x), dtype=torch.int32)
         graph = Data(x=x, edge_index=None)
         graph.n_pulses = n_pulses
-        graph.features = self._features
+        graph.features = self._features[1:]
 
         # Write attributes, either target labels, truth info or original features.
-        add_these_to_graph = [truth_dict]  # [labels_dict, truth_dict]
+        add_these_to_graph = [labels_dict, truth_dict]
         if node_truth is not None:
             add_these_to_graph.append(node_truth_dict)
         for write_dict in add_these_to_graph:
@@ -238,7 +285,35 @@ class Dataset(ABC, torch.utils.data.Dataset, LoggerMixin):
                         f"Could not assign `{key}` with type '{type(value).__name__}' as attribute to graph."
                     )
 
+        # Additionally add original features as (static) attributes
+        for ix, feature in enumerate(graph.features):
+            graph[feature] = graph.x[:, ix].detach()
+
         return graph
+
+    def _get_labels(self, truth_dict: Dict[str, Any]) -> Dict[str, Any]:
+        """Return dictionary of custom labels to be added to graph as attributed."""
+        abs_pid = abs(truth_dict["pid"])
+        sim_type = truth_dict["sim_type"]
+
+        labels_dict = {
+            "event_no": truth_dict["event_no"],
+            "muon": int(abs_pid == 13),
+            "muon_stopped": int(truth_dict.get("stopped_muon") == 1),
+            "noise": int((abs_pid == 1) & (sim_type != "data")),
+            "neutrino": int(
+                (abs_pid != 13) & (abs_pid != 1)
+            ),  # @TODO: `abs_pid in [12,14,16]`?
+            "v_e": int(abs_pid == 12),
+            "v_u": int(abs_pid == 14),
+            "v_t": int(abs_pid == 16),
+            "track": int(
+                (abs_pid == 14) & (truth_dict["interaction_type"] == 1)
+            ),
+            "dbang": self._get_dbang_label(truth_dict),
+            "corsika": int(abs_pid > 20),
+        }
+        return labels_dict
 
     def _get_dbang_label(self, truth_dict: Dict[str, Any]) -> int:
         """Get label for double-bang classification."""
