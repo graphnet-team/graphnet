98ccfb1ca2a0a673eae21ee34af2925240ee368d
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-08-19 15:25:26 +0200

    Add specialised casting for pulse series map-type objects; refactor utilities

--- src/graphnet/data/extractors/i3genericextractor.py
+++ src/graphnet/data/extractors/i3genericextractor.py
@@ -1,10 +1,21 @@
-import inspect
 from collections.abc import MutableMapping
-from typing import Any, Dict, List, Optional, OrderedDict
+import inspect
+from typing import Any, Dict, List, Optional
+
 from graphnet.data.extractors.i3extractor import I3Extractor
+from graphnet.data.extractors.utilities.types import (
+    is_boost_enum,
+    is_icecube_class,
+    is_method,
+    is_type,
+)
+from graphnet.data.extractors.utilities.frames import (
+    get_om_keys_and_pulseseries,
+)
 from graphnet.utilities.logging import get_logger
 
 logger = get_logger()
+
 try:
     from icecube import (
         dataclasses,
@@ -20,28 +31,6 @@ class NonFlattenableDict(dict):
 
 
 # Utility function(s)
-def is_boost_enum(obj: Any) -> bool:
-    """Check whether `obj` inherits from Boost.Python.enum."""
-    for cls in type(obj).__bases__:
-        if "Boost.Python.enum" in str(cls):
-            return True
-    return False
-
-
-def is_icecube_class(obj: Any) -> bool:
-    classname = str(type(obj))
-    return "icecube." in classname
-
-
-def is_type(obj: Any) -> bool:
-    """Checks whether `obj` is a type, and not an instance."""
-    return type(obj).__name__ == "type"
-
-
-def is_method(obj: Any) -> bool:
-    return inspect.ismethod(obj) or "Boost.Python.function" in str(type(obj))
-
-
 def flatten_nested_dictionary(
     results: Dict, parent_key: str = "", separator: str = "."
 ) -> Dict:
@@ -80,6 +69,77 @@ class I3GenericExtractor(I3Extractor):
             raise ValueError("No I3Frame currently being processed.")
         return self._frame
 
+    def __call__(self, frame: "icetray.I3Frame") -> dict:
+        """Extract all possible data from `frame`."""
+
+        results = {}
+        self._frame = frame
+        for key in [
+            "I3Triggers",
+            # "I3TriggerHierarchy"
+            # "I3MCTree",
+        ]:  # frame.keys():
+            try:
+                obj = frame[key]
+            except (KeyError, RuntimeError):
+                self.logger.debug(f"Frame {key} not supported. Skipping.")
+                continue
+
+            # Special case(s)
+            if isinstance(
+                obj,
+                (
+                    dataclasses.I3DOMLaunchSeriesMap,
+                    dataclasses.I3RecoPulseSeriesMap,
+                    dataclasses.I3RecoPulseSeriesMapMask,
+                    dataclasses.I3RecoPulseSeriesMapUnion,
+                ),
+            ):
+                self.logger.info(f"Got pulse series - {key}!")
+                result = self._cast_pulse_series_to_pure_python(frame, key)
+
+            elif isinstance(
+                obj,
+                (
+                    dataclasses.I3MapKeyUInt,
+                    dataclasses.I3MapKeyDouble,
+                    dataclasses.I3MapKeyVectorInt,
+                    dataclasses.I3MapKeyVectorDouble,
+                ),
+            ):
+                self.logger.info(f"Got per-pulse map - {key}!")
+                result = self._cast_pulse_series_to_pure_python(frame, key)
+
+                if result is None:
+                    self.logger.debug(
+                        f"Per-pulse map {key} didn't return anything."
+                    )
+                    # result = {}
+
+                else:
+                    # If we get a per-pulse map, which isn't a
+                    # "I3RecoPulseSeriesMap*", we don't care about area,
+                    # direction, orientation, and position -- we only care
+                    # about the OM index for future reference. We therefore
+                    # only keep these indices and the associated mapping value.
+                    keep_keys = ["value"] + [
+                        key_ for key_ in result if key_.startswith("index.")
+                    ]
+                    result = {key_: result[key_] for key_ in keep_keys}
+
+            # Generic case
+            else:
+
+                self.logger.info(f"Got generic object {key} in frame.")
+                print(type(obj))
+                result = self._cast_object_to_pure_python(obj)
+
+            results[key] = flatten_nested_dictionary(result)
+
+        self._frame = None
+
+        return results
+
     def _get_member_variables(
         self, obj: Any, return_discarded: bool = False
     ) -> List[str]:
@@ -103,21 +163,19 @@ class I3GenericExtractor(I3Extractor):
 
             if is_type(value):
                 discarded_member_variables["is_type"].append(attr)
-                continue
-            if is_method(value):
+            elif is_method(value):
                 discarded_member_variables["is_method"].append(attr)
-                continue
-            if is_boost_enum(value):
+            elif is_boost_enum(value):
                 discarded_member_variables["is_boost_enum"].append(attr)
-                continue
-            valid_member_variables.append(attr)
+            else:
+                valid_member_variables.append(attr)
 
         if return_discarded:
             return valid_member_variables, discarded_member_variables
 
         return valid_member_variables
 
-    def _extract(self, obj: Any) -> Any:
+    def _cast_object_to_pure_python(self, obj: Any) -> Any:
         """Extracts all possible pure-python data from `obj`."""
 
         self.logger.debug(f"Value: {obj}")
@@ -126,10 +184,13 @@ class I3GenericExtractor(I3Extractor):
         if not is_icecube_class(obj):
             self.logger.debug("Found non-I3 class. Exiting.")
             if isinstance(obj, (list, tuple, set)):
-                return [self._extract(element) for element in obj]
+                return [
+                    self._cast_object_to_pure_python(element)
+                    for element in obj
+                ]
             elif isinstance(obj, dict):
                 return {
-                    str(key): self._extract(value)
+                    str(key): self._cast_object_to_pure_python(value)
                     for key, value in obj.items()
                 }
             else:
@@ -139,11 +200,13 @@ class I3GenericExtractor(I3Extractor):
             member_variables,
             discarded_member_variables,
         ) = self._get_member_variables(obj, return_discarded=True)
+
         self.logger.debug(
             f"Found the following member variables: {member_variables}"
         )
         self.logger.debug(
-            f"Discarded the following member variables: {discarded_member_variables}"
+            "Discarded the following member variables: "
+            f"{discarded_member_variables}"
         )
 
         results = None  # Default
@@ -156,23 +219,28 @@ class I3GenericExtractor(I3Extractor):
                 self.logger.debug(
                     f"Calling `extract` on valid member attribute: {attr}"
                 )
-                result = self._extract(value)
+                result = self._cast_object_to_pure_python(value)
                 results[attr] = result
 
         else:
             self.logger.debug(
-                "Found no member variables! Trying to check for python object-like signatures."
+                "Found no member variables! Trying to check for python "
+                "object-like signatures."
             )
 
             # Dict-like
             if hasattr(obj, "items"):
+                self.logger.critial("Is dict-like")
                 # Call function again
-                results = NonFlattenableDict(self._extract(dict(obj)))
+                results = NonFlattenableDict(
+                    self._cast_object_to_pure_python(dict(obj))
+                )
 
             # List-like
             elif hasattr(obj, "__len__") and hasattr(obj, "__getitem__"):
+                self.logger.critial("Is list-like")
                 # Call function again
-                results = self._extract(list(obj))
+                results = self._cast_object_to_pure_python(list(obj))
 
             else:
                 self.logger.warning(
@@ -182,86 +250,74 @@ class I3GenericExtractor(I3Extractor):
 
         return results
 
-    def __call__(self, frame: icetray.I3Frame) -> dict:
-        """Extract all possible data from `frame`."""
-
-        results = {}
-        self._frame = frame
-        for key in ["IceCubePulses"]:  # frame.keys():  # TEMP!!!
-            try:
-                obj = frame[key]
-            except (KeyError, RuntimeError):
-                self.logger.debug(f"Frame {key} not supported. Skipping.")
-                continue
-
-            self.logger.debug(f"Calling `extract` on key {key} in frame.")
-            results[key] = flatten_nested_dictionary(self._extract(obj))
-
-        self._frame = None
-
-        return results
-
-        """
-        # Get OM data
-        om_keys, data = self._get_om_keys_and_pulseseries(frame)
-
-        for om_key in om_keys:
-            # Common values for each OM
-            x = self._gcd_dict[om_key].position.x
-            y = self._gcd_dict[om_key].position.y
-            z = self._gcd_dict[om_key].position.z
-            area = self._gcd_dict[om_key].area
-            rde = self._get_relative_dom_efficiency(frame, om_key)
-
-            # Loop over pulses for each OM
-            pulses = data[om_key]
-            for pulse in pulses:
-                output["charge"].append(pulse.charge)
-                output["dom_time"].append(pulse.time)
-                output["width"].append(pulse.width)
-                output["pmt_area"].append(area)
-                output["rde"].append(rde)
-                output["dom_x"].append(x)
-                output["dom_y"].append(y)
-                output["dom_z"].append(z)
-
-        return output"""
-
-    def _get_om_keys_and_pulseseries(self, frame):
-        """Gets the indicies for the gcd_dict and the pulse series
+    def _cast_pulse_series_to_pure_python(
+        self,
+        frame: "icetray.I3Frame",
+        key: str,
+    ) -> Optional[Dict[str, List[Any]]]:
+        """Cast pulse series `key` to a pure-python data representation.
 
         Args:
-            frame (i3 physics frame): i3 physics frame
+            frame (icetray.I3Frame): I3 physics frame.
+            key (str): Name of the pulse series to be cast.
 
         Returns:
-            om_keys (index): the indicies for the gcd_dict
-            data    (??)   : the pulse series
+            Dict[str, List[Any]]: Dictionary of lists of properties for each
+                pulse across optical modules (OMs), if any pulses are found.
+            None, otherwise
         """
-        try:
-            data = frame[self._pulsemap]
-        except KeyError:
-            self.logger.error(
-                f"Pulsemap {self._pulsemap} does not exist in `frame`"
-            )
-            raise
+        om_keys, data = get_om_keys_and_pulseseries(
+            frame,
+            key,
+            self._calibration,
+        )
 
-        try:
-            om_keys = data.keys()
-        except:  # noqa: E722
-            try:
-                if "I3Calibration" in frame.keys():
-                    data = frame[self._pulsemap].apply(frame)
-                    om_keys = data.keys()
-                else:
-                    frame["I3Calibration"] = self._calibration
-                    data = frame[self._pulsemap].apply(frame)
-                    om_keys = data.keys()
-                    del frame[
-                        "I3Calibration"
-                    ]  # Avoid adding unneccesary data to frame
-            except:  # noqa: E722
-                data = dataclasses.I3RecoPulseSeriesMap.from_frame(
-                    frame, self._pulsemap
-                )
-                om_keys = data.keys()
-        return om_keys, data
+        result = []
+        for om_key in om_keys:
+            om_data = self._cast_object_to_pure_python(self._gcd_dict[om_key])
+
+            # Remove all "orientation.*"-type keys. They provide no
+            # information apart from the (hopefully!) standard
+            # coordinate system and the OM direction, which is covered
+            # by the "direction.*" keys anyway.
+            om_data.pop("orientation", None)
+
+            om_indices = self._cast_object_to_pure_python(om_key)
+            om_data["index"] = om_indices
+
+            om_data = flatten_nested_dictionary(om_data)
+
+            pulses = data[om_key]
+
+            if len(pulses) == 0:
+                continue
+
+            pulse_data = self._cast_object_to_pure_python(pulses)
+
+            # Ensure that `pulse_data` has the form of a list of dictionary of
+            # per-pulse properties
+            if isinstance(pulse_data, (list, tuple)):
+                if not isinstance(pulse_data[0], dict):
+                    pulse_data = [{"value": value} for value in pulse_data]
+            else:
+                pulse_data = [{"value": pulse_data}]
+
+            for ix in range(len(pulse_data)):
+                pulse_data[ix].update(om_data)
+
+            # "Transpose" list of dicts to dict of lists
+            pulse_data = {
+                key: [pulse[key] for pulse in pulse_data]
+                for key in pulse_data[0]
+            }
+            result.append(pulse_data)
+
+        # Concatenate list of pulses from different OMs
+        if len(result):
+            result = {
+                key: [pulse for pulses in result for pulse in pulses[key]]
+                for key in result[0]
+            }
+            return result
+        else:
+            return None
