3ddce57c6b86e4f2c664d2775700eeb078a168fc
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-08-30 21:55:23 +0200

    Make available imports in graphnet.data.{sqlite,parquet} conditional on whether 'torch' is installed

--- src/graphnet/data/sqlite/__init__.py
+++ src/graphnet/data/sqlite/__init__.py
@@ -1,4 +1,10 @@
+from graphnet.utilities.imports import has_torch_package
+
 from .sqlite_dataconverter import SQLiteDataConverter
-from .sqlite_dataset import SQLiteDataset
-from .sqlite_dataset_perturbed import SQLiteDatasetPerturbed
 from .sqlite_utilities import run_sql_code, save_to_sql
+
+if has_torch_package():
+    from .sqlite_dataset import SQLiteDataset
+    from .sqlite_dataset_perturbed import SQLiteDatasetPerturbed
+
+del has_torch_package
