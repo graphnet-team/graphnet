fc38bda47046e7da29cf3f7f33f05965e3b6514a
Rasmus Ørsøe <rahn@outlook.dk>
2022-09-07 14:16:49 +0200

    changed imports

--- src/graphnet/data/extractors/__init__.py
+++ src/graphnet/data/extractors/__init__.py
@@ -2,3 +2,6 @@ from .i3extractor import *
 from .i3featureextractor import *
 from .i3truthextractor import *
 from .i3retroextractor import *
+from .i3splinempeextractor import *
+from .i3tumextractor import *
+
