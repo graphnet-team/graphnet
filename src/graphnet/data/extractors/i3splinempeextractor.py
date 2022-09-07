cba9e8edcd81268dd25b58a0780b810665ea9729
Rasmus Ørsøe <rahn@outlook.dk>
2022-09-07 15:19:42 +0200

    last polish

--- src/graphnet/data/extractors/i3splinempeextractor.py
+++ src/graphnet/data/extractors/i3splinempeextractor.py
@@ -1,6 +1,4 @@
 from graphnet.data.extractors.i3extractor import I3Extractor
-import numpy as np
-
 
 class I3SplineMPEICExtractor(I3Extractor):
     def __init__(self, name="spline_mpe_ic"):
@@ -9,18 +7,15 @@ class I3SplineMPEICExtractor(I3Extractor):
     def __call__(self, frame) -> dict:
         """Extracts TUM DNN Recos and associated variables"""
         output = {}
-        if self._frame_contains_retro(frame):
+        if "SplineMPEIC" in frame:
             output.update(
                 {
                     "zenith_spline_mpe_ic": frame[
                         "SplineMPEIC"
-                    ].dir.zenith.value,
+                    ].dir.zenith,
                     "azimuth_spline_mpe_ic": frame[
                         "SplineMPEIC"
-                    ].dir.zenith.value,
-                    "pbfErr1_spline_mpe_ic": frame[
-                        "SplineMPEICParaboloidFitParams"
-                    ].pbfErr1.value,
+                    ].dir.azimuth,
                 }
             )
 
