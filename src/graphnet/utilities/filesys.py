b55e3a99ede1f1fcfa5a642469b070fcf6f5d6b4
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-09-23 11:44:30 +0200

    Update is_gcd_file to match more GCD files; and allow for I3 and GCD files with different suffixes/compression methods in the same folder in find_i3_files

--- src/graphnet/utilities/filesys.py
+++ src/graphnet/utilities/filesys.py
@@ -9,14 +9,17 @@ from typing import List, Optional, Union
 
 def is_gcd_file(filename: str) -> bool:
     """Checks whether `filename` is a GCD file."""
-    if re.search("(gcd|geo)", filename.lower()):
+    if (
+        re.search("(gcd|geo)", filename.lower())
+        or "GeoCalibDetector" in filename
+    ):
         return True
     return False
 
 
 def is_i3_file(filename: str) -> bool:
     """Checks whether `filename` is an I3 file."""
-    if is_gcd_file(filename.lower()):
+    if is_gcd_file(filename):
         return False
     elif has_extension(filename, ["bz2", "zst", "gz"]):
         return True
@@ -57,27 +60,31 @@ def find_i3_files(
     for directory in directories:
 
         # Recursively find all I3-like files in `directory`.
+        paths = []
         i3_patterns = ["*.bz2", "*.zst", "*.gz"]
         for i3_pattern in i3_patterns:
-            paths = list(Path(directory).rglob(i3_pattern))
+            paths.extend(list(Path(directory).rglob(i3_pattern)))
 
-            # Loop over all folders containing such I3-like files.
-            folders = sorted(set([os.path.dirname(path) for path in paths]))
-            for folder in folders:
+        # Loop over all folders containing such I3-like files.
+        folders = sorted(set([path.parent for path in paths]))
+        for folder in folders:
 
-                # List all I3 and GCD files, respectively, in the current folder.
-                folder_files = glob(os.path.join(folder, i3_pattern))
-                folder_i3_files = list(filter(is_i3_file, folder_files))
-                folder_gcd_files = list(filter(is_gcd_file, folder_files))
+            # List all I3 and GCD files, respectively, in the current folder.
+            folder_files = [
+                str(path) for path in paths if path.parent == folder
+            ]
+            folder_i3_files = list(filter(is_i3_file, folder_files))
+            folder_gcd_files = list(filter(is_gcd_file, folder_files))
 
-                # Make sure that no more than one GCD file is found; and use rescue file of none is found.
-                assert len(folder_gcd_files) <= 1
-                if len(folder_gcd_files) == 0:
-                    folder_gcd_files = [gcd_rescue]
+            # Make sure that no more than one GCD file is found; and use rescue file of none is found.
+            assert len(folder_gcd_files) <= 1
+            if len(folder_gcd_files) == 0:
+                folder_gcd_files = [gcd_rescue]
 
-                # Store list of I3 files and corresponding GCD files.
-                folder_gcd_files = folder_gcd_files * len(folder_i3_files)
-                gcd_files.extend(folder_gcd_files)
-                i3_files.extend(folder_i3_files)
+            # Store list of I3 files and corresponding GCD files.
+            folder_gcd_files = folder_gcd_files * len(folder_i3_files)
+
+            gcd_files.extend(folder_gcd_files)
+            i3_files.extend(folder_i3_files)
 
     return i3_files, gcd_files
