8826ef3ad6988ff26874d180c77e05428f4ffa78
Rasmus Ørsøe <rahn@outlook.dk>
2022-09-30 09:38:08 +0200

    filing for divorce with black

--- src/graphnet/data/extractors/i3hybridrecoextractor.py
+++ src/graphnet/data/extractors/i3hybridrecoextractor.py
@@ -1,5 +1,6 @@
 from graphnet.data.extractors.i3extractor import I3Extractor
 
+
 class I3GalacticPlaneHybridRecoExtractor(I3Extractor):
     def __init__(self, name="dnn_hybrid"):
         super().__init__(name)
@@ -9,19 +10,31 @@ class I3GalacticPlaneHybridRecoExtractor(I3Extractor):
         output = {}
         if "DNNCascadeAnalysis_version_001_p00" in frame:
             reco_object = frame["DNNCascadeAnalysis_version_001_p00"]
-            keys = ["angErr" , "angErr_uncorrected" ,"dec" ,"dpsi" ,"energy" ,"event" ,"ra" ,"run" ,"subevent" ,"time","trueDec","trueE","trueRa","true_azi","true_zen"]
+            keys = [
+                "angErr",
+                "angErr_uncorrected",
+                "dec",
+                "dpsi",
+                "energy",
+                "event",
+                "ra",
+                "run",
+                "subevent",
+                "time",
+                "trueDec",
+                "trueE",
+                "trueRa",
+                "true_azi",
+                "true_zen",
+            ]
             for key in keys:
-                output.update(
-                    {
-                    key: reco_object[key]
-                    }
-                )
+                output.update({key: reco_object[key]})
             output.update(
-                    {
-                    'zenith_hybrid': reco_object["zen"],
-                    'azimuth_hybrid': reco_object["azi"],
-                    'energy_hybrid_log': reco_object["logE"]
-                    }
-                )
+                {
+                    "zenith_hybrid": reco_object["zen"],
+                    "azimuth_hybrid": reco_object["azi"],
+                    "energy_hybrid_log": reco_object["logE"],
+                }
+            )
 
         return output
