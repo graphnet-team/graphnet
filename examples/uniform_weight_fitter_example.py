4440c91b8068c9afed034fe524d5325b2fef0198
Rasmus Oersoe <rahn@outlook.dk>
2022-10-04 14:09:48 +0200

    created training dir, moved fitter

--- examples/uniform_weight_fitter_example.py
+++ examples/uniform_weight_fitter_example.py
@@ -2,7 +2,7 @@ import sqlite3
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from graphnet.models.weight_fitting import UniformWeightFitter
+from graphnet.training.weight_fitting import UniformWeightFitter
 
 
 def get_truth(db):
