176a2546f36c959a857136434df0a15f06053979
Rasmus Oersoe <rahn@outlook.dk>
2022-10-04 16:11:13 +0200

    docs

--- src/graphnet/training/weight_fitting.py
+++ src/graphnet/training/weight_fitting.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import sqlite3
-
+from typing import Optional, List
 from graphnet.data.utils import run_sql_code, save_to_sql
 
 
@@ -22,23 +22,31 @@ class UniformWeightFitter:
 
     def fit_weights(
         self,
-        bins,
-        variable,
-        weight_name=None,
-        add_to_database=False,
-        selection=None,
-    ):
-        """Calculates pr. event weights such that the distribution of variable is uniform for events in selection. If selection is None, the weights are calculated using all events in the database.
+        bins: np.ndarray,
+        variable: str,
+        weight_name: Optional[str] = None,
+        add_to_database: bool = False,
+        selection: Optional[List[int]] = None,
+    ) -> pd.DataFrame:
+        """Calculate per-event weights to make `variable` distribution uniform.
+
+        If selection is None, the weights are calculated using all events in the
+        database.
 
         Args:
-            bins (numpy.array): An array containing the bins. E.g. numpy.arange(0,180,1)
+            bins (numpy.array): An array containing the bins. E.g.
+                numpy.arange(0,180,1)
             variable (str): the name of the truth variable in truth_table
-            weight_name (str, optional): the assigned column name of the weights if add_to_database = True. Defaults to None.
-            add_to_database (bool, optional): If True, the weights are written to the database as a new table named weight_name . Defaults to False.
-            selection (List, optional): a List containing the event_no's to which the weights are calculated. Defaults to None.
+            weight_name (str, optional): the assigned column name of the weights
+                if add_to_database = True. Defaults to None.
+            add_to_database (bool, optional): If True, the weights are written
+                to the database as a new table named weight_name. Defaults to
+                False.
+            selection (List, optional): a List containing the event_no's to
+                which the weights are calculated. Defaults to None.
 
         Returns:
-            _type_: pandas.DataFrame
+            pandas.DataFrame
         """
         truth = self._get_truth(variable, selection)
         # Histogram `truth_values`
@@ -60,7 +68,8 @@ class UniformWeightFitter:
             save_to_sql(truth, weight_name, self._database_path)
         return truth.sort_values("event_no").reset_index(drop=True)
 
-    def _get_truth(self, variable, selection):
+    def _get_truth(self, variable: str, selection: Optional[List[int]] = None):
+        """Return truth `variable`, optionally only for `selection` event nos."""
         if selection is None:
             query = f"select {self._index_column}, {variable} from {self._truth_table}"
         else:
