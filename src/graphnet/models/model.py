999b2ac14e385bb01093d1fa68b241847b0748de
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-10-06 14:27:35 +0200

    Add abstract forward method to Model

--- src/graphnet/models/model.py
+++ src/graphnet/models/model.py
@@ -1,9 +1,16 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Union
 
 from pytorch_lightning import LightningModule
+from torch import Tensor
+from torch_geometric.data import Data
 
 from graphnet.utilities.logging import LoggerMixin
 
 
 class Model(LightningModule, LoggerMixin, ABC):
     """Base class for all models in graphnet."""
+
+    @abstractmethod
+    def forward(self, x: Union[Tensor, Data]) -> Union[Tensor, Data]:
+        """Forward pass."""
