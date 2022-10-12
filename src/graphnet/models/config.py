75cf2db9b9573afb9f0c1c8bd163ad0a2361ef31
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-10-12 16:22:00 +0200

    Better handle nested models and non-graphnet classes

--- src/graphnet/models/config.py
+++ src/graphnet/models/config.py
@@ -1,8 +1,9 @@
 """Config classes for the `graphnet.data` module."""
+from ast import arguments
 from functools import wraps
 import inspect
 import types
-from typing import Any, Callable, Dict, Optional, OrderedDict
+from typing import Any, Callable, Dict, Union, Optional, OrderedDict
 
 from pydantic import BaseModel
 import ruamel.yaml as yaml
@@ -15,6 +16,23 @@ from graphnet.utilities.logging import LoggerMixin
 CONFIG_FILES_SUFFIXES = (".yml", ".yaml")
 
 
+def traverse_and_apply(
+    obj: Any, fn: Callable, fn_kwargs: Optional[Dict[str, Any]] = None
+):
+    """Apply `fn` to all elements in `obj`, resulting in same structure."""
+    if isinstance(obj, (list, tuple)):
+        return [traverse_and_apply(elem, fn, fn_kwargs) for elem in obj]
+    elif isinstance(obj, dict):
+        return {
+            key: traverse_and_apply(val, fn, fn_kwargs)
+            for key, val in obj.items()
+        }
+    else:
+        if fn_kwargs is None:
+            fn_kwargs = {}
+        return fn(obj, **fn_kwargs)
+
+
 class ModelConfig(BaseModel, LoggerMixin):
     """Config for data conversion and ingestion."""
 
@@ -24,17 +42,35 @@ class ModelConfig(BaseModel, LoggerMixin):
     def __init__(self, **data: Any) -> None:
         """Constructor."""
         # Parse any nested `ModelConfig` arguments
+        def _is_model_config_entry(entry: Dict[str, Any]) -> bool:
+            return (
+                isinstance(entry, dict)
+                and len(entry) == 1
+                and self.__class__.__name__ in entry
+            )
+
+        def _parse_model_config_entry(
+            entry: Dict[str, Any]
+        ) -> Union["ModelConfig", Any]:
+            """Parse dictionary entry to `ModelConfig`."""
+            assert _is_model_config_entry(entry)
+            config_dict = entry[self.__class__.__name__]
+            config = self.__class__(**config_dict)
+            return config
+
         for arg in data["arguments"]:
             value = data["arguments"][arg]
-            if (
-                isinstance(value, dict)
-                and len(value) == 1
-                and self.__class__.__name__ in value
-            ):
-                nested_config_dict = value[self.__class__.__name__]
-                nested_config = self.__class__(**nested_config_dict)
-                data["arguments"][arg] = nested_config
+            if _is_model_config_entry(value):
+                data["arguments"][arg] = _parse_model_config_entry(value)
 
+            elif isinstance(value, (tuple, list)):
+                for ix, elem in enumerate(value):
+                    if _is_model_config_entry(elem):
+                        data["arguments"][arg][ix] = _parse_model_config_entry(
+                            elem
+                        )
+
+        # Base class constructor
         super().__init__(**data)
 
     @classmethod
@@ -44,7 +80,7 @@ class ModelConfig(BaseModel, LoggerMixin):
             CONFIG_FILES_SUFFIXES
         ), "Please specify YAML config file."
         with open(path, "r") as f:
-            config_dict = yaml.load(f)
+            config_dict = yaml.safe_load(f)
 
         return cls(**config_dict)
 
@@ -76,23 +112,65 @@ class ModelConfig(BaseModel, LoggerMixin):
 
         # Parse potential ModelConfig arguments
         arguments = dict(**self.arguments)
-        for arg, value in arguments.items():
-            if isinstance(value, ModelConfig):
-                arguments[arg] = value.construct_model()
-            elif isinstance(value, str) and value.startswith("!lambda"):
-                if trust:
-                    arguments[arg] = eval(value[1:])
-                else:
-                    self.logger.error(
-                        "Constructing model containing a lambda function "
-                        f"({value}) with `trust=False` if you trust the lambda "
-                        "functions in this ModelConfig, set `trust=True` and "
-                        "reconstruct the model again."
-                    )
-                    raise ValueError
+        arguments = traverse_and_apply(
+            arguments,
+            self._deserialise,
+            fn_kwargs={"trust": trust},
+        )
 
         # Construct model based on arguments
         return namespace_classes[self.class_name](**arguments)
+
+    @classmethod
+    def _deserialise(cls, obj: Any, trust: bool = False) -> Any:
+        if isinstance(obj, ModelConfig):
+            return obj.construct_model(trust=trust)
+
+        elif isinstance(obj, str) and obj.startswith("!lambda"):
+            if trust:
+                return eval(obj[1:])
+            else:
+                raise ValueError(
+                    "Constructing model containing a lambda function "
+                    f"({obj}) with `trust=False`. If you trust the lambda "
+                    "functions in this ModelConfig, set `trust=True` and "
+                    "reconstruct the model again."
+                )
+
+        elif isinstance(obj, str) and obj.startswith("!class"):
+            if trust:
+                module, class_name = obj.split()[1:]
+                eval(f"from {module} import {class_name}")
+                return eval(class_name)
+            else:
+                raise ValueError(
+                    f"Constructing model containing a class ({obj}) with"
+                    "`trust=False`. If you trust the class definitions in this "
+                    "ModelConfig, set `trust=True` and reconstruct the model "
+                    "again."
+                )
+
+        else:
+            return obj
+
+    @classmethod
+    def _serialise(cls, obj: Any) -> Any:
+        """Serialise `obj` to a format that can be saved to file."""
+        if isinstance(obj, ModelConfig):
+            return obj._as_dict()
+        elif isinstance(obj, type):
+            return f"!class {obj.__module__} {obj.__name__}"
+        elif isinstance(obj, Callable):
+            if hasattr(obj, "__name__") and obj.__name__ == "<lambda>":
+                return "!" + inspect.getsource(obj).split("=")[1].strip("\n ,")
+            else:
+                raise ValueError(
+                    f"Object `{obj}` is callable but not a lambda function. "
+                    "Please wrap in a lambda function to allow for saving "
+                    "this function verbatim in a model config file."
+                )
+
+        return obj
 
     def _as_dict(self) -> Dict[str, Dict[str, Any]]:
         """Represent ModelConfig as a dict.
@@ -102,28 +180,24 @@ class ModelConfig(BaseModel, LoggerMixin):
         themselves models.
         """
         config_dict = self.dict()
-        for arg, obj in self.arguments.items():
-            if isinstance(obj, ModelConfig):
-                config_dict["arguments"][arg] = obj._as_dict()
-            if isinstance(obj, Callable):
-                if obj.__name__ == "<lambda>":
-                    config_dict["arguments"][arg] = "!" + inspect.getsource(
-                        obj
-                    ).split("=")[1].strip("\n ,")
-                else:
-                    self.logger.error(
-                        f"Argument `{arg}` to `{self.class_name}` is callable "
-                        "but not a lambda function. Please wrap in a lambda "
-                        "function to allow for saving this function verbatim "
-                        "in a model config file."
-                    )
-                    raise ValueError
+        config_dict["arguments"] = traverse_and_apply(
+            self.arguments, self._serialise
+        )
 
         return {self.__class__.__name__: config_dict}
 
 
 def save_config(init_fn: Callable):
     """Save the arguments to `__init__` functions as member `ModelConfig`."""
+
+    def _serialise(obj: Any) -> Any:
+        """Serialise `obj` to a format that works for `ModelConfig`"""
+        if isinstance(obj, Model):
+            return obj.config
+        elif isinstance(obj, type):
+            return str(obj)
+        else:
+            return obj
 
     @wraps(init_fn)
     def wrapper(self, *args, **kwargs):
@@ -147,12 +221,8 @@ def save_config(init_fn: Callable):
         # Add keyword arguments
         cfg.update(kwargs)
 
-        # Handle nested `Model`s
-        for key, val in list(cfg.items()):
-            if isinstance(val, Model):
-                cfg[key] = val.config
-            elif isinstance(val, type):
-                cfg[key] = str(val)
+        # Handle nested `Model`s, etc.
+        cfg = traverse_and_apply(cfg, _serialise)
 
         # Add `ModelConfig` as member variables
         self._config = ModelConfig(
