2a692906a6a23eec4fc7b56a5fa922c38f1bab60
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-10-14 10:45:02 +0200

    Add docstring

--- src/graphnet/data/__init__.py
+++ src/graphnet/data/__init__.py
@@ -1,5 +1,4 @@
 """Modules for converting and ingesting data.
 
-`graphnet.data` enables converting domain-specific data to industry-
-standard, intermediate file formats and reading this data.
+`graphnet.data` enables converting domain-specific data to industry-standard, intermediate file formats and reading this data.
 """
