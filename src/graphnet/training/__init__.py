8294c8cee31e5bc61e37e8822470a7e3dd8cad2f
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-10-28 12:15:13 +0200

    Add docstring

---
+++ src/graphnet/training/__init__.py
@@ -0,0 +1,8 @@
+"""Modules for training models.
+
+`graphnet.models` allows for configuring and building complex GNN models using
+simple, physics-oriented components. This module provides modular components
+subclassing `torch.nn.Module`, meaning that users only need to import a few,
+existing, purpose-built components and chain them together to form a complete
+GNN
+"""
