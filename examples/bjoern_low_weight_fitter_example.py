97ae502fa1323bcc5879157b5a4c03c7004b76ea
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-11-03 10:17:14 +0100

    Remove unused import

--- examples/bjoern_low_weight_fitter_example.py
+++ examples/bjoern_low_weight_fitter_example.py
@@ -1,7 +1,6 @@
 """Example of how to use WeightFitter class, specifically BjoernLow."""
 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 from graphnet.training.weight_fitting import BjoernLow
