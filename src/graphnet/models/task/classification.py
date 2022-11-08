d133f7cbc0dd3e3cbb7fe18fb6633add9fb993cc
Morten Holm <Volumunox@gmail.com>
2022-11-08 12:03:33 +0100

    moving multiclassification to classification tasks

--- src/graphnet/models/task/classification.py
+++ src/graphnet/models/task/classification.py
@@ -1,20 +1,28 @@
 """Classification-specific `Model` class(es)."""
 
-import torch
 from torch import Tensor
+from torch.nn import Softmax
+from graphnet.models.config import save_config
 
 from graphnet.models.task import Task
 
-
-class BinaryClassificationTask(Task):
-    """Performs binary classification."""
-
-    # Requires one feature, logit for being signal class.
-    nb_inputs = 1
+class ClassificationTask(Task):
+    # Requires the same number of features as the number of classes being predicted
+    @save_config
+    def __init__(self, nb_inputs: int, *args, **kwargs):
+        self._nb_inputs = nb_inputs
+        super().__init__(*args, **kwargs)
+        
+        self._softmax = Softmax()
+    
+    @property
+    def nb_inputs(self):
+        """Number of outputs from Classification."""
+        return self._nb_inputs
 
     def _forward(self, x: Tensor) -> Tensor:
-        # transform probability of being muon
-        return torch.sigmoid(x)
+        """Transform latent features into probabilities."""
+        return self._softmax(x)
 
 
 class BinaryClassificationTaskLogits(Task):
