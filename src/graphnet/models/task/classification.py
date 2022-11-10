ffe7701f2555275ca58cd98cd06e6f075091afe5
Morten Holm <Volumunox@gmail.com>
2022-11-10 11:26:54 +0100

    change 'nb_inputs' to 'nb_classes'

--- src/graphnet/models/task/classification.py
+++ src/graphnet/models/task/classification.py
@@ -9,15 +9,15 @@ from graphnet.models.task import Task
 class ClassificationTask(Task):
     # Requires the same number of features as the number of classes being predicted
     @save_config
-    def __init__(self, nb_inputs: int, *args, **kwargs):
-        self._nb_inputs = nb_inputs
+    def __init__(self, nb_classes, *args, **kwargs):
+        self._nb_classes = nb_classes
         super().__init__(*args, **kwargs)
         self._softmax = Softmax()
     
     @property
     def nb_inputs(self):
         """Number of outputs from Classification."""
-        return self._nb_inputs
+        return self._nb_classes
 
     def _forward(self, x: Tensor) -> Tensor:
         """Transform latent features into probabilities."""
