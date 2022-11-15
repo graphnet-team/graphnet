4e533ecc3af3cee62ecf0092d3ac4e825cb4db05
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-11-15 10:41:31 +0100

    Add collate_fn as argument to DataLoader, to make pytorch-lightning happy

--- src/graphnet/data/dataloader.py
+++ src/graphnet/data/dataloader.py
@@ -1,6 +1,6 @@
 """Base `Dataloader` class(es) used in `graphnet`."""
 
-from typing import Any, List
+from typing import Any, Callable, List
 
 import torch.utils.data
 from torch_geometric.data import Batch, Data
@@ -8,28 +8,30 @@ from torch_geometric.data import Batch, Data
 from graphnet.data.dataset import Dataset
 
 
+def collate_fn(graphs: List[Data]) -> Batch:
+    """Remove graphs with less than two DOM hits.
+
+    Should not occur in "production.
+    """
+    graphs = [g for g in graphs if g.n_pulses > 1]
+    return Batch.from_data_list(graphs)
+
+
 class DataLoader(torch.utils.data.DataLoader):
     """Class for loading data from a `Dataset`."""
 
-    def ___init__(
+    def __init__(
         self,
         dataset: Dataset,
         batch_size: int,
         shuffle: bool,
         num_workers: int = 10,
         persistent_workers: bool = True,
+        collate_fn: Callable = collate_fn,
         **kwargs: Any,
     ) -> None:
         """Construct `DataLoader`."""
-
-        def collate_fn(graphs: List[Data]) -> Batch:
-            """Remove graphs with less than two DOM hits.
-
-            Should not occur in "production.
-            """
-            graphs = [g for g in graphs if g.n_pulses > 1]
-            return Batch.from_data_list(graphs)
-
+        # Base class constructor
         super().__init__(
             dataset,
             batch_size=batch_size,
