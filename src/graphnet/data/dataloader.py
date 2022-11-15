7ad329092e8fd66289b5f0e92acfb902ee555ce3
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-11-15 13:31:10 +0100

    Add DataLoader.from_dataset_config method

--- src/graphnet/data/dataloader.py
+++ src/graphnet/data/dataloader.py
@@ -1,11 +1,12 @@
 """Base `Dataloader` class(es) used in `graphnet`."""
 
-from typing import Any, Callable, List
+from typing import Any, Callable, Dict, List, Union
 
 import torch.utils.data
 from torch_geometric.data import Batch, Data
 
 from graphnet.data.dataset import Dataset
+from graphnet.utilities.config import DatasetConfig
 
 
 def collate_fn(graphs: List[Data]) -> Batch:
@@ -15,6 +16,11 @@ def collate_fn(graphs: List[Data]) -> Batch:
     """
     graphs = [g for g in graphs if g.n_pulses > 1]
     return Batch.from_data_list(graphs)
+
+
+def do_shuffle(selection_name: str) -> bool:
+    """Check whether to shuffle selection with name `selection_name`."""
+    return selection_name.lower().startswith("train")
 
 
 class DataLoader(torch.utils.data.DataLoader):
@@ -42,3 +48,39 @@ class DataLoader(torch.utils.data.DataLoader):
             prefetch_factor=2,
             **kwargs,
         )
+
+    @classmethod
+    def from_dataset_config(
+        cls,
+        config: DatasetConfig,
+        batch_size: int,
+        **kwargs: Any,
+    ) -> Union["DataLoader", Dict[str, "DataLoader"]]:
+        """Construct `DataLoader`s based on selections in `DatasetConfig`."""
+        if isinstance(config.selection, dict):
+            assert "shuffle" not in kwargs, (
+                "When passing a `DatasetConfig` with multiple selections, "
+                "`shuffle` is automatically inferred from the selection name, "
+                "and thus should not specified as an argument."
+            )
+            datasets = Dataset.from_config(config)
+            assert isinstance(datasets, dict)
+            data_loaders: Dict[str, DataLoader] = {}
+            for name, dataset in datasets.items():
+                data_loaders[name] = cls(
+                    dataset,
+                    batch_size=batch_size,
+                    shuffle=do_shuffle(name),
+                    **kwargs,
+                )
+
+            return data_loaders
+
+        else:
+            assert "shuffle" in kwargs, (
+                "When passing a `DatasetConfig` with a single selections, you "
+                "need to specify `shuffle` as an argument."
+            )
+            dataset = Dataset.from_config(config)
+            assert isinstance(dataset, Dataset)
+            return cls(dataset, batch_size=batch_size, **kwargs)
