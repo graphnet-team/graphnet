e28f0111a883e499b5c00f39d34594c1834757cc
Morten Holm <Volumunox@gmail.com>
2022-11-28 20:15:49 +0100

    restructure

--- examples/train_classification_model.py
+++ examples/train_classification_model.py
@@ -1,17 +1,18 @@
 """Example of training Model."""
 
 import os
-from typing import cast
+from typing import cast, Dict, Any
 import numpy as np
 
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping
 
-# from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.loggers import WandbLogger
 import torch
 from torch.optim.adam import Adam
+from torch.nn.functional import softmax
 
-from graphnet.training.loss_functions import NLLLoss
+from graphnet.training.loss_functions import CrossEntropyLoss
 from graphnet.data.constants import FEATURES, TRUTH
 from graphnet.data.sqlite.sqlite_selection import (
     get_desired_event_numbers,
@@ -28,9 +29,9 @@ from graphnet.training.utils import (
     save_results,
 )
 
-# from graphnet.utilities.logging import get_logger
+from graphnet.utilities.logging import get_logger
 
-# logger = get_logger()
+logger = get_logger()
 
 # Configurations
 torch.multiprocessing.set_sharing_strategy("file_system")
@@ -44,55 +45,18 @@ WANDB_DIR = "./wandb/"
 os.makedirs(WANDB_DIR, exist_ok=True)
 
 # Initialise Weights & Biases (W&B) run
-# wandb_logger = WandbLogger(
-#    project="example-script",
-#    entity="graphnet-team",
-#    save_dir=WANDB_DIR,
-#    log_model=True,
-# )
+wandb_logger = WandbLogger(
+    project="example-script",
+    entity="graphnet-team",
+    save_dir=WANDB_DIR,
+    log_model=True,
+)
 
 
-def main() -> None:
-    """Run example."""
-    # logger.info(f"features: {features}")
-    # logger.info(f"truth: {truth}")
-
-    # single integer definition of class
-    # class_options = 3
-    # list of classes
-    # class_options = [0,1,2]
-    # transformation of target to a given class integer
-    class_options = {
-        1: 0,
-        -1: 0,
-        13: 1,
-        -13: 1,
-        12: 2,
-        -12: 2,
-        14: 2,
-        -14: 2,
-        16: 2,
-        -16: 2,
-    }
-
-    # Configuration
-    config = {
-        "db": "/groups/icecube/petersen/GraphNetDatabaseRepository/Leon_MC_data/last_one_lvl3MC.db",
-        "pulsemap": "SplitInIcePulses",
-        "batch_size": 512,
-        "num_workers": 10,
-        "accelerator": "cpu",  # gpu
-        "devices": 1,  # [0],
-        "target": "pid",
-        "classification": class_options,
-        "n_epochs": 10,
-        "patience": 5,
-    }
-    archive = "/groups/icecube/petersen/GraphNetDatabaseRepository/example_results/train_classification_model"
-    run_name = "dynedge_{}_example".format(config["target"])
-
+def train(config: Dict[str, Any]) -> None:
+    """Train model with configuration given by `config`."""
     # Log configuration to W&B
-    # wandb_logger.experiment.config.update(config)
+    wandb_logger.experiment.config.update(config)
 
     # Common variables
     train_selection = get_desired_event_numbers(
@@ -104,6 +68,9 @@ def main() -> None:
         fraction_nu_mu=float(1 / 9),
         fraction_nu_tau=float(1 / 9),
     )
+
+    logger.info(f"features: {features}")
+    logger.info(f"truth: {truth}")
 
     (
         training_dataloader,
@@ -127,10 +94,11 @@ def main() -> None:
         global_pooling_schemes=["min", "max", "mean", "sum"],
     )
     task = ClassificationTask(
-        nb_classes=len(np.unique(list(class_options.values()))),
+        nb_classes=len(np.unique(list(config["class_options"].values()))),
         hidden_size=gnn.nb_outputs,
         target_labels=config["target"],
-        loss_function=NLLLoss(options=config["classification"]),
+        loss_function=CrossEntropyLoss(options=config["class_options"]),
+        transform_inference=lambda x: softmax(x, dim=-1),
     )
     model = StandardModel(
         detector=detector,
@@ -167,13 +135,13 @@ def main() -> None:
         max_epochs=config["n_epochs"],
         callbacks=callbacks,
         log_every_n_steps=1,
-        # logger=wandb_logger,
+        logger=wandb_logger,
     )
 
     try:
         trainer.fit(model, training_dataloader, validation_dataloader)
     except KeyboardInterrupt:
-        # logger.warning("[ctrl+c] Exiting gracefully.")
+        logger.warning("[ctrl+c] Exiting gracefully.")
         pass
 
     # Saving predictions to file
@@ -189,7 +157,54 @@ def main() -> None:
         additional_attributes=[cast(str, config["target"]), "event_no"],
     )
 
-    save_results(cast(str, config["db"]), run_name, results, archive, model)
+    save_results(
+        cast(str, config["db"]),
+        config["run_name"],
+        results,
+        config["archive"],
+        model,
+    )
+
+
+def main() -> None:
+    """Run example."""
+    # transformation of target to a given class integer
+    class_options = {
+        1: 0,
+        -1: 0,
+        13: 1,
+        -13: 1,
+        12: 2,
+        -12: 2,
+        14: 2,
+        -14: 2,
+        16: 2,
+        -16: 2,
+    }
+
+    target = "pid"
+
+    archive = "/groups/icecube/petersen/GraphNetDatabaseRepository/example_results/train_classification_model"
+    run_name = "dynedge_{}_example".format(target)
+
+    # Configuration
+    config = {
+        "db": "/groups/icecube/petersen/GraphNetDatabaseRepository/Leon2022_DataAndMC_CSVandDB_StoppedMuons/last_one_lvl3MC.db",
+        "pulsemap": "SplitInIcePulses",
+        "batch_size": 512,
+        "num_workers": 10,
+        "accelerator": "cpu",  # gpu
+        "devices": 1,  # [0],
+        "target": target,
+        "class_options": class_options,
+        "n_epochs": 2,
+        "patience": 1,
+        "archive": archive,
+        "run_name": run_name,
+        "max_events": 50000,
+        "node_pooling": True,
+    }
+    train(config)
 
 
 if __name__ == "__main__":
