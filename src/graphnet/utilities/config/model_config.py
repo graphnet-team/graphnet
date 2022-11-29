17c00aa446d24f0f62bc21f51c82d7a80abde1db
Andreas Søgaard <andreas.sogaard@gmail.com>
2022-11-29 12:43:02 +0100

    Allow for saving regular, and not just lambda, functions to config file

--- src/graphnet/utilities/config/model_config.py
+++ src/graphnet/utilities/config/model_config.py
@@ -26,6 +26,11 @@ if TYPE_CHECKING:
     from graphnet.models import Model
 
 
+FUNCTION_DEFINITION_PATTERN = (
+    r"^def (?P<function_name>[a-zA-Z]{1}[a-zA-Z0-9_]+) *\(.*\) *:"
+)
+
+
 class ModelConfig(BaseConfig):
     """Configuration for all `Model`s."""
 
@@ -146,6 +151,22 @@ class ModelConfig(BaseConfig):
                     "reconstruct the model again."
                 )
 
+        elif isinstance(obj, str) and obj.startswith("!function"):
+            if trust:
+                source = obj[10:]
+                match = re.match(FUNCTION_DEFINITION_PATTERN, source)
+                assert match
+                exec(source)
+                fn = eval(match.group("function_name"))
+                return fn
+            else:
+                raise ValueError(
+                    f"Constructing model containing a function ({obj}) with "
+                    "`trust=False`. If you trust the functions in this "
+                    "ModelConfig, set `trust=True` and reconstruct the model "
+                    "again."
+                )
+
         elif isinstance(obj, str) and obj.startswith("!class"):
             if trust:
                 module, class_name = obj.split()[1:]
@@ -173,11 +194,20 @@ class ModelConfig(BaseConfig):
             if hasattr(obj, "__name__") and obj.__name__ == "<lambda>":
                 return "!" + inspect.getsource(obj).split("=")[1].strip("\n ,")
             else:
-                raise ValueError(
-                    f"Object `{obj}` is callable but not a lambda function. "
-                    "Please wrap in a lambda function to allow for saving "
-                    "this function verbatim in a model config file."
-                )
+                try:
+                    source = inspect.getsource(obj)
+                    match = re.match(FUNCTION_DEFINITION_PATTERN, source)
+                    if match and match.group("function_name"):
+                        return f"!function {source}"
+                    else:
+                        raise ValueError
+                except (TypeError, ValueError):
+                    raise ValueError(
+                        f"Object `{obj}` is callable but not a lambda or regular "
+                        "function. Please wrap in a, e.g., lambda function to "
+                        "allow for saving this function verbatim in a model "
+                        "config file."
+                    )
 
         return obj
 
