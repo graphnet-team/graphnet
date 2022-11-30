34602359d3a2309ed503b2342c044ad01e57cf95
Morten Holm <Volumunox@gmail.com>
2022-11-30 11:44:21 +0100

    removed unused module dependencies

--- tests/models/test_task.py
+++ tests/models/test_task.py
@@ -7,10 +7,7 @@ from graphnet.models.detector.icecube import IceCube86
 from graphnet.models.graph_builders import KNNGraphBuilder
 from graphnet.models.gnn import DynEdge
 from graphnet.models.task.reconstruction import EnergyReconstruction
-from graphnet.models.task.classification import ClassificationTask
 from graphnet.training.loss_functions import LogCoshLoss
-
-import numpy as np
 
 
 def test_transform_prediction_and_target() -> None:
