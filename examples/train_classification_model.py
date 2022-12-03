ab7b3a0eb35130934095de8bb5dc63248b887c55
Morten Holm <Volumunox@gmail.com>
2022-12-03 18:50:15 +0100

    restructure to fit new reduced training example

--- examples/train_classification_model.py
+++ examples/train_classification_model.py
@@ -1,44 +1,19 @@
 """Example of training Model."""
 
 import os
-from typing import cast, Dict, Any
-import numpy as np
+from typing import Dict, Any
 
-from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping
-
 from pytorch_lightning.loggers import WandbLogger
-import torch
-from torch.optim.adam import Adam
-from torch.nn.functional import softmax
 
-from graphnet.training.loss_functions import CrossEntropyLoss
-from graphnet.data.constants import FEATURES, TRUTH
-from graphnet.data.sqlite.sqlite_selection import (
-    get_desired_event_numbers,
-)
-from graphnet.models import StandardModel
-from graphnet.models.detector.icecube import IceCubeDeepCore
-from graphnet.models.gnn import DynEdge
-from graphnet.models.graph_builders import KNNGraphBuilder
-from graphnet.models.task.classification import MulticlassClassificationTask
+from graphnet.data.dataloader import DataLoader
+from graphnet.models import Model
 from graphnet.training.callbacks import ProgressBar
-from graphnet.training.utils import (
-    get_predictions,
-    make_train_validation_dataloader,
-    save_results,
+from graphnet.utilities.config import (
+    DatasetConfig,
+    ModelConfig,
+    TrainingConfig,
 )
-
-from graphnet.utilities.logging import get_logger
-
-logger = get_logger()
-
-# Configurations
-torch.multiprocessing.set_sharing_strategy("file_system")
-
-# Constants
-features = FEATURES.DEEPCORE
-truth = TRUTH.DEEPCORE[:-1]
 
 # Make sure W&B output directory exists
 WANDB_DIR = "./wandb/"
@@ -53,105 +28,79 @@ wandb_logger = WandbLogger(
 )
 
 
-def train(config: Dict[str, Any]) -> None:
+def train(general_config: Dict[str, Any]) -> None:
     """Train model with configuration given by `config`."""
+    # Configuration
+    config = TrainingConfig(
+        target="pid",
+        early_stopping_patience=5,
+        fit={"gpus": [0], "max_epochs": 5},
+        dataloader={"batch_size": 512, "num_workers": 10},
+    )
+
+    run_name = "dynedge_{}_classification_example".format(config.target)
+
     # Log configuration to W&B
     wandb_logger.experiment.config.update(config)
 
-    # Common variables; equal distribution of pid.
-    train_selection = get_desired_event_numbers(
-        database=cast(str, config["db"]),
-        desired_size=cast(int, config["event_numbers"]),
-        fraction_noise=float(1 / 3),
-        fraction_muon=float(1 / 3),
-        fraction_nu_e=float(1 / 9),
-        fraction_nu_mu=float(1 / 9),
-        fraction_nu_tau=float(1 / 9),
+    # Construct dataloaders
+    dataset_config = DatasetConfig.load(
+        "configs/datasets/" + general_config["dataset"] + ".yml"
     )
+    # dataloader_test, dataloader_valid, ..
+    dataloaders = DataLoader.from_dataset_config(
+        dataset_config,
+        **config.dataloader,
+    )
+    wandb_logger.experiment.config.update(dataset_config.as_dict())
 
-    logger.info(f"features: {features}")
-    logger.info(f"truth: {truth}")
-
-    (
-        training_dataloader,
-        validation_dataloader,
-    ) = make_train_validation_dataloader(
-        cast(str, config["db"]),
-        train_selection,
-        cast(str, config["pulsemap"]),
-        features,
-        truth,
-        batch_size=cast(int, config["batch_size"]),
-        num_workers=cast(int, config["num_workers"]),
-    )
-
-    # Building model
-    detector = IceCubeDeepCore(
-        graph_builder=KNNGraphBuilder(nb_nearest_neighbours=8),
-    )
-    gnn = DynEdge(
-        nb_inputs=detector.nb_outputs,
-        global_pooling_schemes=["min", "max", "mean", "sum"],
-    )
-    task = MulticlassClassificationTask(
-        nb_classes=len(np.unique(list(config["class_options"].values()))),
-        hidden_size=gnn.nb_outputs,
-        target_labels=config["target"],
-        loss_function=CrossEntropyLoss(options=config["class_options"]),
-        transform_inference=lambda x: softmax(x, dim=-1),
-    )
-    model = StandardModel(
-        detector=detector,
-        gnn=gnn,
-        tasks=[task],
-        optimizer_class=Adam,
-        optimizer_kwargs={"lr": 1e-04, "eps": 1e-03},
-    )
+    # Build model
+    model_config = ModelConfig.load(f"configs/models/{run_name}.yml")
+    model = Model.from_config(model_config, trust=True)
+    wandb_logger.experiment.config.update(model_config.as_dict())
 
     # Training model
     callbacks = [
         EarlyStopping(
             monitor="val_loss",
-            patience=config["patience"],
+            patience=config.early_stopping_patience,
         ),
         ProgressBar(),
     ]
 
-    trainer = Trainer(
-        accelerator=config["accelerator"],
-        devices=config["devices"],
-        max_epochs=config["n_epochs"],
+    model.fit(
+        dataloaders["train"],
+        dataloaders["validation"],
         callbacks=callbacks,
-        log_every_n_steps=1,
         logger=wandb_logger,
+        **config.fit,
     )
 
-    try:
-        trainer.fit(model, training_dataloader, validation_dataloader)
-    except KeyboardInterrupt:
-        logger.warning("[ctrl+c] Exiting gracefully.")
-        pass
+    # Get predictions
+    if isinstance(config.target, str):
+        prediction_columns = [
+            config.target + "_noise_pred",
+            config.target + "_muon_pred",
+            config.target + "_neutrino_pred",
+        ]
+        additional_attributes = [config.target]
+    else:
+        prediction_columns = [target + "_pred" for target in config.target]
+        additional_attributes = config.target
 
-    # Saving predictions to file
-    results = get_predictions(
-        trainer,
-        model,
-        validation_dataloader,
-        [
-            cast(str, config["target"]) + "_noise_pred",
-            cast(str, config["target"]) + "_muon_pred",
-            cast(str, config["target"]) + "_neutrino_pred",
-        ],
-        additional_attributes=[cast(str, config["target"]), "event_no"],
+    results = model.predict_as_dataframe(
+        dataloaders["test"],
+        prediction_columns=prediction_columns,
+        additional_attributes=additional_attributes + ["event_no"],
     )
 
-    save_results(
-        cast(str, config["db"]),
-        config["run_name"],
-        results,
-        config["archive"],
-        model,
-    )
+    # Save predictions and model to file
+    db_name = dataset_config.path.split("/")[-1].split(".")[0]
+    path = os.path.join(general_config["archive"], db_name, run_name)
+
+    results.to_csv(f"{path}/results.csv")
+    model.save_state_dict(f"{path}/state_dict.pth")
+    model.save(f"{path}/model.pth")
 
 
 def main() -> None:
@@ -170,29 +119,16 @@ def main() -> None:
         -16: 2,
     }
 
-    target = "pid"
-
-    archive = "/groups/icecube/petersen/GraphNetDatabaseRepository/example_results/train_classification_model"
-    run_name = "dynedge_{}_example".format(target)
-
-    # Configuration
-    config = {
+    # General configuration
+    general_config = {
         "db": "/groups/icecube/petersen/GraphNetDatabaseRepository/Leon2022_DataAndMC_CSVandDB_StoppedMuons/last_one_lvl3MC.db",
-        "pulsemap": "SRTInIcePulses",
-        "batch_size": 512,
-        "num_workers": 10,
-        "accelerator": "gpu",
-        "devices": [1],
-        "target": target,
-        "event_numbers": 1000,
+        "dataset": "PID_classification_last_one_lvl3MC.yml",
+        "model": "dynedge_PID_Classification_noise_muon_neutrino_example.yml",
         "class_options": class_options,
-        "n_epochs": 50,
-        "patience": 5,
-        "archive": archive,
-        "run_name": run_name,
+        "archive": "/groups/icecube/petersen/GraphNetDatabaseRepository/example_results/train_classification_model",
     }
 
-    train(config)
+    train(general_config)
 
 
 if __name__ == "__main__":
