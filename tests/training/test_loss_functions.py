62e35ce06b76084b04a23ebd12341e4cb0a667b4
Morten Holm <33733987+MortenHolmRep@users.noreply.github.com>
2022-12-03 14:25:37 +0100

    Update tests/training/test_loss_functions.py
    Co-authored-by: Andreas Søgaard <andreas.sogaard@gmail.com>

--- tests/training/test_loss_functions.py
+++ tests/training/test_loss_functions.py
@@ -6,10 +6,7 @@ import torch
 from torch import Tensor
 from torch.autograd import grad
 
-from graphnet.training.loss_functions import (
-    LogCoshLoss,
-    VonMisesFisherLoss,
-)
+from graphnet.training.loss_functions import LogCoshLoss, VonMisesFisherLoss
 from graphnet.utilities.maths import eps_like
 
 
