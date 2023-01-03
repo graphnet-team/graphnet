03f3a2021075adf10fded460ac46e8b003735f7a
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-01-03 13:13:24 +0100

    Make script configurable

--- examples/02_data/03_convert_parquet_to_sqlite.py
+++ examples/02_data/03_convert_parquet_to_sqlite.py
@@ -5,20 +5,17 @@ from graphnet.data.utilities.parquet_to_sqlite import ParquetToSQLiteConverter
 from graphnet.utilities.argparse import ArgumentParser
 
 
-def main() -> None:
+def main(parquet_path: str, mc_truth_table: str) -> None:
     """Run example."""
-    # Path to parquet file or directory containing parquet files
-    parquet_path = TEST_PARQUET_DATA
-
     # Path to where you want the database to be stored
     outdir = f"{TEST_DATA_DIR}/output/convert_parquet_to_sqlite/"
 
     # Name of the database.
-    # Will be saved in outdir/database_name/data/database_name.db
-    database_name = TEST_PARQUET_DATA.split("/")[-1].split(".")[0]
+    # Will be saved in {outdir}/{database_name}/data/{database_name}.db
+    database_name = parquet_path.split("/")[-1].split(".")[0]
 
     converter = ParquetToSQLiteConverter(
-        mc_truth_table="truth",
+        mc_truth_table=mc_truth_table,
         parquet_path=parquet_path,
     )
     converter.run(outdir=outdir, database_name=database_name)
@@ -33,6 +30,19 @@ Convert Parquet files to SQLite database.
 """
     )
 
+    parser.add_argument(
+        "--parquet-path",
+        action="store",
+        help="Path to Parquet file (default: %(default)s)",
+        default=TEST_PARQUET_DATA,
+    )
+    parser.add_argument(
+        "--mc-truth-table",
+        action="store",
+        help="Name of MC truth table in Parquet file (default: %(default)s)",
+        default="truth",
+    )
+
     args = parser.parse_args()
 
-    main()
+    main(args.parquet_path, args.mc_truth_table)
