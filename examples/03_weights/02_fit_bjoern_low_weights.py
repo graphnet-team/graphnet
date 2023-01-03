18e368f293905ad7b33668b05c171fbfbcc6f829
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-01-03 10:23:17 +0100

    Update example

--- examples/03_weights/02_fit_bjoern_low_weights.py
+++ examples/03_weights/02_fit_bjoern_low_weights.py
@@ -3,13 +3,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from graphnet.constants import TEST_SQLITE_DATA
 from graphnet.training.weight_fitting import BjoernLow
+from graphnet.utilities.argparse import ArgumentParser
 
 
 def main() -> None:
     """Run example."""
     # Configuration
-    database = "/my_databases/my_database/data/my_database.db"
+    database = TEST_SQLITE_DATA
     variable = "energy"
     bins = bins = np.arange(0, 5, 0.01)  # in log10
 
@@ -18,20 +20,31 @@ def main() -> None:
     weights = fitter.fit(
         bins=bins,
         variable=variable,
-        add_to_database=True,
+        add_to_database=False,
         transform=np.log10,
         x_low=1.5,
     )
+    print(weights.head())
 
     # Plot the results
     fig = plt.figure()
     plt.hist(
         weights["energy"],
         bins=bins,
-        weights=weights["energy_bjørn_low_weight"],
+        weights=weights["energy_bjoern_low_weight"],
     )
-    fig.savefig("test_hist.png")
+    fig.savefig("test_hist_weighting_bjoern.png")
 
 
 if __name__ == "__main__":
+
+    # Parse command-line arguments
+    parser = ArgumentParser(
+        description="""
+Fit per-event weights according to the `BjoernLow` weight fitter.
+"""
+    )
+
+    args = parser.parse_args()
+
     main()
