6bb3729854e59f99f3f509177727abd448150371
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-01-10 14:33:53 +0100

    Add strandard arguments

--- src/graphnet/utilities/argparse.py
+++ src/graphnet/utilities/argparse.py
@@ -1,7 +1,7 @@
 """Consistent CLI argument parsing across `graphnet`."""
 
 import argparse
-from typing import Optional
+from typing import List, Optional
 
 
 ASCII_LOGO = r"""
@@ -35,3 +35,66 @@ class ArgumentParser(argparse.ArgumentParser):
             description=description,
             formatter_class=argparse.RawTextHelpFormatter,
         )
+
+    def with_standard_arguments(self, *args: str) -> "ArgumentParser":
+        """Add standard, named arguments to the `ArgumentParser`."""
+        remaining = list(args)
+
+        if "gpus" in remaining:
+            self.add_argument(
+                "--gpus",
+                nargs="+",
+                type=int,
+                help=(
+                    "Indices of GPUs to use for training (default: "
+                    "%(default)s)"
+                ),
+            )
+            remaining.remove("gpus")
+
+        if "max-epochs":
+            self.add_argument(
+                "--max-epochs",
+                type=int,
+                help=(
+                    "Maximum number of epochs to train (default: %(default)s)"
+                ),
+                default=5,
+            )
+            remaining.remove("max-epochs")
+
+        if "early-stopping-patience" in remaining:
+            self.add_argument(
+                "--early-stopping-patience",
+                type=int,
+                help=(
+                    "Number of epochs with no improvement in validation loss "
+                    "after which to stop training (default: %(default)s)"
+                ),
+                default=5,
+            )
+            remaining.remove("early-stopping-patience")
+
+        if "batch-size":
+            self.add_argument(
+                "--batch-size",
+                type=int,
+                help="Batch size to use for training (default: %(default)s)",
+                default=128,
+            )
+            remaining.remove("batch-size")
+
+        if "num-workers" in remaining:
+            self.add_argument(
+                "--num-workers",
+                type=int,
+                help="Number of workers to fetch data (default: %(default)s)",
+                default=10,
+            )
+            remaining.remove("num-workers")
+
+        assert (
+            len(remaining) == 0
+        ), f"The following arguments weren't resolved: {remaining}"
+
+        return self
