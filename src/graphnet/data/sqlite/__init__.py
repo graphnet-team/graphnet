5debe8a3c6365b3f67078f97c9e9dccd97dc94a6
Rasmus Ørsøe <rahn@outlook.dk>
2023-01-12 13:31:41 +0100

    unit test fix

--- src/graphnet/data/sqlite/__init__.py
+++ src/graphnet/data/sqlite/__init__.py
@@ -5,6 +5,6 @@ from graphnet.utilities.imports import has_torch_package
 from .sqlite_dataconverter import SQLiteDataConverter
 from .sqlite_utilities import create_table_and_save_to_sql
 
-# from .sqlite_dataset import SQLiteDataset
-# from .sqlite_dataset_perturbed import SQLiteDatasetPerturbed
+from .sqlite_dataset import SQLiteDataset
+from .sqlite_dataset_perturbed import SQLiteDatasetPerturbed
 from .sqlite_utilities import run_sql_code, save_to_sql
