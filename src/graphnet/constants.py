638129e51f770a1d5c7b2fc8cbec8d3040865d16
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-01-20 14:25:08 +0100

    Move example and test data files

--- src/graphnet/constants.py
+++ src/graphnet/constants.py
@@ -6,8 +6,12 @@ GRAPHNET_ROOT_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..", "..")
 )
 
+# Data directory
+DATA_DIR = os.path.join(GRAPHNET_ROOT_DIR, "data")
+
 # Test data
-TEST_DATA_DIR = os.path.join(GRAPHNET_ROOT_DIR, "test_data")
+TEST_DATA_DIR = os.path.join(DATA_DIR, "tests")
+TEST_OUTPUT_DIR = os.path.join(TEST_DATA_DIR, "output")
 
 _test_dataset_name = "oscNext_genie_level7_v02"
 _test_dataset_file = f"{_test_dataset_name}_first_5_frames"
@@ -20,6 +24,10 @@ TEST_PARQUET_DATA = os.path.join(
     _test_dataset_name,
     f"{_test_dataset_file}.parquet",
 )
+
+# Example data
+EXAMPLE_DATA_DIR = os.path.join(DATA_DIR, "examples")
+EXAMPLE_OUTPUT_DIR = os.path.join(EXAMPLE_DATA_DIR, "outputs")
 
 # Configuration files
 CONFIG_DIR = os.path.join(GRAPHNET_ROOT_DIR, "configs")
