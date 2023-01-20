126291de055983c4d4fa2e319f52eb944ec604c2
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-01-20 14:06:56 +0100

    Capture all target names, and add option to names these manually

--- examples/04_training/01_train_model.py
+++ examples/04_training/01_train_model.py
@@ -34,6 +34,7 @@ def main(
     early_stopping_patience: int,
     batch_size: int,
     num_workers: int,
+    prediction_names: Optional[List[str]],
 ) -> None:
     """Run example."""
     # Initialise Weights & Biases (W&B) run
@@ -50,7 +51,9 @@ def main(
 
     # Configuration
     config = TrainingConfig(
-        target=model._tasks[0]._target_labels[0],
+        target=[
+            target for task in model._tasks for target in task._target_labels
+        ],
         early_stopping_patience=early_stopping_patience,
         fit={
             "gpus": gpus,
@@ -60,7 +63,7 @@ def main(
     )
 
     archive = os.path.join(TEST_DATA_DIR, "output", "train_model")
-    run_name = "dynedge_{}_example".format(config.target)
+    run_name = "dynedge_{}_example".format("_".join(config.target))
 
     # Construct dataloaders
     dataset_config = DatasetConfig.load(dataset_config_path)
@@ -102,6 +105,12 @@ def main(
         prediction_columns = [target + "_pred" for target in config.target]
         additional_attributes = config.target
 
+    if prediction_names:
+        prediction_columns = prediction_names
+
+    print(f"config.target: {config.target}")
+    print(f"prediction_columns: {prediction_columns}")
+
     results = model.predict_as_dataframe(
         dataloaders["test"],
         prediction_columns=prediction_columns,
@@ -128,23 +137,21 @@ Train GNN model.
 """
     )
 
-    parser.add_argument(
-        "--dataset-config",
-        help="Path to dataset config file (default: %(default)s)",
-        default="configs/datasets/training_example_data_sqlite.yml",
-    )
-    parser.add_argument(
-        "--model-config",
-        help="Path to model config file (default: %(default)s)",
-        default="configs/models/example_model.yml",
-    )
-
     parser.with_standard_arguments(
+        "dataset-config",
+        "model-config",
         "gpus",
         ("max-epochs", 5),
         "early-stopping-patience",
         ("batch-size", 16),
         "num-workers",
+    )
+
+    parser.add_argument(
+        "--prediction-names",
+        nargs="+",
+        help="Names of each prediction output feature (default: %(default)s)",
+        default=None,
     )
 
     args = parser.parse_args()
@@ -157,4 +164,5 @@ Train GNN model.
         args.early_stopping_patience,
         args.batch_size,
         args.num_workers,
+        args.prediction_names,
     )
