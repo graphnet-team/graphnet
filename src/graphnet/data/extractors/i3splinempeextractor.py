079d4199d0f33a492ac34050e81fa3a25a4e4314
Aske-Rosted <askerosted@gmail.com>
2023-01-23 16:32:09 +0900

    Add extractor for comparisons to other algorithms

--- src/graphnet/data/extractors/i3splinempeextractor.py
+++ src/graphnet/data/extractors/i3splinempeextractor.py
@@ -1,6 +1,6 @@
 """I3Extractor class(es) for extracting SplineMPE reconstruction."""
 
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, List
 
 from graphnet.data.extractors.i3extractor import I3Extractor
 
@@ -24,6 +24,28 @@ class I3SplineMPEICExtractor(I3Extractor):
                 {
                     "zenith_spline_mpe_ic": frame["SplineMPEIC"].dir.zenith,
                     "azimuth_spline_mpe_ic": frame["SplineMPEIC"].dir.azimuth,
+                }
+            )
+
+        return output
+
+
+class I3ComparisonExtractor(I3Extractor):
+    """Class for extracting pointing predictions from various algorithms."""
+
+    def __init__(self, name: str):
+        """Construct I3ComparisonExtractor."""
+        # Base class constructor
+        super().__init__(name)
+
+    def __call__(self, frame: "icetray.I3Frame") -> Dict[str, float]:
+        """Extract pointing predictions."""
+        output = {}
+        if self._name in frame:
+            output.update(
+                {
+                    "zenith_" + self._name: frame[self._name].dir.zenith,
+                    "azimuth_" + self._name: frame[self._name].dir.azimuth,
                 }
             )
 
