89e622cb9ccc6beb3bc6d87099edceab8202b600
Morten Holm <Volumunox@gmail.com>
2023-02-07 10:35:32 +0100

    changed model and added all prometheus examples

--- examples/04_training/01_train_models.sh
+++ examples/04_training/01_train_models.sh
@@ -12,32 +12,35 @@ bash_directory=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
 dataset_config=$(realpath "$bash_directory/../../configs/datasets/training_example_data_sqlite.yml")
 # what GPU to use; more information can be gained with the module nvitop
 gpus=0
-# the maximum number of epochs; this greatly affect learning rate scheduling
+# the maximum number of epochs; if used, this greatly affect learning rate scheduling
 max_epochs=5
 # early stopping threshold
 early_stopping_patience=5
 # events in a batch
 batch_size=16
 # number of CPUs to use
-num_workers=1
+num_workers=2
 
 ## Model dependent; applies to each model in sequence
 # path to model files in the GraphNeT directory
 model_directory=$(realpath "$bash_directory/../../configs/models")
 # list of model configurations to train
 declare -a model_configs=(
-    "${model_directory}/dynedge_energy_example.yml"
-    "${model_directory}/dynedge_position_example.yml"
+    "${model_directory}/example_direction_reconstruction_model.yml"
+    "${model_directory}/example_energy_reconstruction_model.yml"
+    "${model_directory}/example_vertex_position_reconstruction_model.yml"
 )
 
 # suffix ending on the created directory
 declare -a suffixs=(
+    "direction"
     "energy"
     "position"
 )
 
 # prediction name outputs per model
 declare -a prediction_names=(
+    "zenith_pred zenith_kappa_pred azimuth_pred azimuth_kappa_pred"
     "energy_pred"
     "position_x_pred position_y_pred position_z_pred"
 )
