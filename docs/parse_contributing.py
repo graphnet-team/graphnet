aa96dc75d6c636399962c9c31edb1ff90570c153
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-02-07 13:15:04 +0100

    Automate parsing of contributing instructions

---
+++ docs/parse_contributing.py
@@ -0,0 +1,46 @@
+"""Script to parse CONTRIBUTING.md file to contrib instructions for website."""
+
+import argparse
+import re
+
+
+def main(input_file: str, output_file: str) -> None:
+    # Check(s)
+    assert input_file != output_file
+    assert input_file.endswith(".md")
+    assert output_file.endswith(".md")
+
+    # Read input file contents
+    with open(input_file, "r") as f:
+        content = f.read()
+
+    pattern = "## Version control"
+    m = re.search(pattern, content)
+    content = content[:m.start()]
+
+    # Trim for whitespaces and newlines
+    content = content.strip()
+
+    # Update relative links for absolute ones
+    content = content.replace("./", "https://github.com/graphnet-team/graphnet/tree/main/")
+
+    # Write parsed results to output file
+    with open(output_file, "w") as f:
+        f.write(content)
+
+
+if __name__ == "__main__":
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(
+        description="""
+Parse CONTRIBUTING.md file to contrib instructions for website.
+"""
+    )
+
+    parser.add_argument("-i", "--input-file", required=True)
+    parser.add_argument("-o", "--output-file", required=True)
+
+    args = parser.parse_args()
+
+    main(args.input_file, args.output_file)
