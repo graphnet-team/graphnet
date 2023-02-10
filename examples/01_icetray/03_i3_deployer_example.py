587cc3488d3095fcaef77fdbbc6b46c9d9ca8fa5
Rasmus Oersoe <rahn@outlook.dk>
2023-02-10 13:09:29 +0100

    simplified example 03

--- examples/01_icetray/03_i3_deployer_example.py
+++ examples/01_icetray/03_i3_deployer_example.py
@@ -8,6 +8,7 @@ from typing import Dict, List
 
 from graphnet.deployment.i3modules import (
     GraphNeTI3Deployer,
+    GraphNeTI3Module,
     I3InferenceModule,
 )
 from graphnet.data.extractors.i3featureextractor import (
@@ -33,85 +34,32 @@ features = FEATURES.UPGRADE
 truth = TRUTH.UPGRADE
 
 
-def construct_mock_model() -> StandardModel:
-    """Construct a mock model for the example.
-
-    Replace this with a trained model.
-    """
-    detector = IceCubeUpgrade(
-        graph_builder=KNNGraphBuilder(nb_nearest_neighbours=8),
-    )
-    gnn = DynEdge(
-        nb_inputs=detector.nb_outputs,
-        global_pooling_schemes=["min", "max", "mean", "sum"],
-    )
-    task = EnergyReconstruction(
-        hidden_size=gnn.nb_outputs,
-        target_labels=["energy"],
-        loss_function=LogCoshLoss(),
-        transform_prediction_and_target=torch.log10,
-    )
-    model = StandardModel(
-        detector=detector,
-        gnn=gnn,
-        tasks=[task],
-        optimizer_class=Adam,
-        optimizer_kwargs={"lr": 1e-03, "eps": 1e-03},
-    )
-    return model
-
-
-def construct_modules(
-    model_dict: Dict[str, Dict], gcd_file: str
-) -> List[I3InferenceModule]:
-    """Construct a list of I3InfereceModules for the I3Deployer."""
-    features = FEATURES.UPGRADE
-    deployment_modules = []
-    for model_name in model_dict.keys():
-        model_path = model_dict[model_name]["model_path"]
-        prediction_columns = model_dict[model_name]["prediction_columns"]
-        pulsemap = model_dict[model_name]["pulsemap"]
-        extractor = I3FeatureExtractorIceCubeUpgrade(pulsemap=pulsemap)
-        deployment_modules.append(
-            I3InferenceModule(
-                pulsemap=pulsemap,
-                features=features,
-                pulsemap_extractor=extractor,
-                model=model_path,
-                gcd_file=gcd_file,
-                prediction_columns=prediction_columns,
-                model_name=model_name,
-            )
-        )
-    return deployment_modules
-
-
 def main() -> None:
     """GraphNeTI3Deployer Example."""
     # configure input files, output folders and pulsemap
     pulsemap = "SplitInIcePulses"
     input_folders = [f"{TEST_DATA_DIR}/i3/upgrade_genie_step4_140028_000998"]
-    output_folder = f"{EXAMPLE_OUTPUT_DIR}/i3_deployment/upgrade"
+    mock_model_path = f"{TEST_DATA_DIR}/models/mock_energy_model.pth"
+    output_folder = f"{EXAMPLE_OUTPUT_DIR}/i3_deployment/upgrade_03_04"
     gcd_file = f"{TEST_DATA_DIR}/i3/upgrade_genie_step4_140028_000998/GeoCalibDetectorStatus_ICUpgrade.v58.mixed.V0.i3.bz2"
     input_files = []
     for folder in input_folders:
         input_files.extend(glob(join(folder, "*.i3.gz")))
 
-    # Configure Module dictionary & construct deployment modules
-    model_dict = {}
-    model_dict["graphnet_dynedge_energy_reconstruction"] = {
-        "model_path": construct_mock_model(),
-        "prediction_columns": ["energy_pred"],
-        "pulsemap": pulsemap,
-    }
-
-    deployment_modules = construct_modules(
-        model_dict=model_dict, gcd_file=gcd_file
+    # Configure Deployment module
+    deployment_module = I3InferenceModule(
+        pulsemap=pulsemap,
+        features=features,
+        pulsemap_extractor=I3FeatureExtractorIceCubeUpgrade(pulsemap=pulsemap),
+        model=mock_model_path,
+        gcd_file=gcd_file,
+        prediction_columns=["energy"],
+        model_name="graphnet_deployment_example",
     )
 
     # Construct I3 deployer
     deployer = GraphNeTI3Deployer(
-        graphnet_modules=deployment_modules,
+        graphnet_modules=[deployment_module],
         n_workers=1,
         gcd_file=gcd_file,
     )
