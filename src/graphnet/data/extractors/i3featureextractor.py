e26a73c59f4fdf2936caf5c7f5473ad2beeeb559
Aske-Rosted <askerosted@gmail.com>
2023-02-21 16:38:41 +0900

    streamlining error handling.

--- src/graphnet/data/extractors/i3featureextractor.py
+++ src/graphnet/data/extractors/i3featureextractor.py
@@ -64,15 +64,15 @@ class I3FeatureExtractorIceCube86(I3FeatureExtractor):
         }
 
         # Get OM data
-        try:
+        if self._pulsemap in frame:
             om_keys, data = get_om_keys_and_pulseseries(
                 frame,
                 self._pulsemap,
                 self._calibration,
             )
-        except KeyError:
-            if self._pulsemap is not None:
-                raise KeyError(f"Pulsemap {self._pulsemap} not in frame")
+        else:
+            warn_once(self, f"Pulsemap {self._pulsemap} not found in frame.")
+            return output
 
         # Added these :
         bright_doms = None
