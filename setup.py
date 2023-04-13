a914098aee0ea7f60124bfd3decf5a3471417e28
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-04-13 15:48:02 +0200

    Restrict pandas to < 2.0 until breaking changes (df.append) are resolved

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ INSTALL_REQUIRES = [
     "dill>=0.3",
     "matplotlib>=3.5",
     "numpy>=1.21",
-    "pandas>=1.3",
+    "pandas>=1.3, <2.0",
     "pyarrow",
     "pydantic",
     "ruamel.yaml",
