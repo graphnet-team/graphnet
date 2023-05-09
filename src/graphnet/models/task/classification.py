742d8d432261ef31be832e776f9ada145e986125
Andreas Søgaard <andreas.sogaard@gmail.com>
2023-05-09 09:38:33 +0200

    Update classification.py

--- src/graphnet/models/task/classification.py
+++ src/graphnet/models/task/classification.py
@@ -24,6 +24,7 @@ class BinaryClassificationTask(Task):
     nb_inputs = 1
     default_target_labels = ["target"]
     default_prediction_labels = ["target_pred"]
+    
     def _forward(self, x: Tensor) -> Tensor:
         # transform probability of being muon
         return torch.sigmoid(x)
@@ -34,6 +35,8 @@ class BinaryClassificationTaskLogits(Task):
 
     # Requires one feature, logit for being signal class.
     nb_inputs = 1
+    default_target_labels = ["target"]
+    default_prediction_labels = ["target_pred"]
 
     def _forward(self, x: Tensor) -> Tensor:
         return x
