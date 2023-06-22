c0bc1c4641dc7c2129c3ea832c04dce19dbf6454
AMHermansen <97125645+AMHermansen@users.noreply.github.com>
2023-06-22 12:20:14 +0200

    Removed python 3.7 from setup

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
     "Intended Audience :: Science/Research",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
