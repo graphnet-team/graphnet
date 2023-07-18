51a0aad58f3daec493082715194770c606e411c4
Rasmus Oersoe <rahn@outlook.dk>
2023-07-18 15:16:44 +0200

    revert

---
+++ src/graphnet/data/dataset/__init__.py
@@ -0,0 +1,14 @@
+"""Dataset classes for training in GraphNeT."""
+# Configuration
+from graphnet.utilities.imports import has_torch_package
+
+if has_torch_package():
+    import torch.multiprocessing
+    from .dataset import EnsembleDataset, Dataset, ColumnMissingException
+    from .parquet.parquet_dataset import ParquetDataset
+    from .sqlite.sqlite_dataset import SQLiteDataset
+    from .sqlite.sqlite_dataset_perturbed import SQLiteDatasetPerturbed
+
+    torch.multiprocessing.set_sharing_strategy("file_system")
+
+del has_torch_package
---
+++ src/graphnet/models/graphs/__init__.py
@@ -0,0 +1,10 @@
+"""Modules for constructing graphs.
+
+´GraphDefinition´ defines the nodes and their features,  and contains general
+graph-manipulation.´EdgeDefinition´ defines how edges are drawn between nodes
+and their features.
+"""
+
+
+from .graph_definition import GraphDefinition
+from .graphs import KNNGraph
