ee5660d44b240f89a327a1ce3fd007ff21522759
Rasmus Oersoe <rahn@outlook.dk>
2023-07-18 16:30:00 +0200

    update dataloader unit tests

--- tests/training/test_dataloader_utilities.py
+++ tests/training/test_dataloader_utilities.py
@@ -10,17 +10,28 @@ import pytest
 
 from graphnet.data.constants import FEATURES, TRUTH
 from graphnet.constants import TEST_PARQUET_DATA, TEST_SQLITE_DATA
+from graphnet.models.graphs import KNNGraph
+from graphnet.models.detector.icecube import IceCubeDeepCore
+from graphnet.models.graphs.nodes import NodesAsPulses
 from graphnet.training.utils import make_train_validation_dataloader
 
 # Configuration
 NB_EVENTS_TOTAL = 5
+
+graph_definition = KNNGraph(
+    detector=IceCubeDeepCore(),
+    node_definition=NodesAsPulses(),
+    nb_nearest_neighbours=8,
+    node_feature_names=FEATURES.DEEPCORE,
+)
 
 
 # Unit test(s)
 def test_none_selection() -> None:
     """Test agreement of the two ways to calculate this loss."""
     (train_dataloader, test_dataloader,) = make_train_validation_dataloader(
-        TEST_SQLITE_DATA,
+        db=TEST_SQLITE_DATA,
+        graph_definition=graph_definition,
         selection=None,
         pulsemaps=["SRTInIcePulses"],
         features=FEATURES.DEEPCORE,
@@ -42,7 +53,8 @@ def test_none_selection() -> None:
 def test_array_selection(selection: Tuple[int]) -> None:
     """Test agreement of the two ways to calculate this loss."""
     (train_dataloader, test_dataloader) = make_train_validation_dataloader(
-        TEST_SQLITE_DATA,
+        db=TEST_SQLITE_DATA,
+        graph_definition=graph_definition,
         selection=list(selection),
         pulsemaps=["SRTInIcePulses"],
         features=FEATURES.DEEPCORE,
@@ -57,7 +69,8 @@ def test_empty_selection() -> None:
     """Test agreement of the two ways to calculate this loss."""
     try:
         _ = make_train_validation_dataloader(
-            TEST_SQLITE_DATA,
+            db=TEST_SQLITE_DATA,
+            graph_definition=graph_definition,
             selection=list(),
             pulsemaps=["SRTInIcePulses"],
             features=FEATURES.DEEPCORE,
@@ -73,7 +86,8 @@ def test_parquet() -> None:
     """Test agreement of the two ways to calculate this loss."""
     try:
         _ = make_train_validation_dataloader(
-            TEST_PARQUET_DATA,
+            db=TEST_PARQUET_DATA,
+            graph_definition=graph_definition,
             selection=None,
             pulsemaps=["SRTInIcePulses"],
             features=FEATURES.DEEPCORE,
