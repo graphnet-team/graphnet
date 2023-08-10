cab8c7441067cbfb1668a4e900dc45cda1f7e024
Rasmus Oersoe <rahn@outlook.dk>
2023-08-10 09:47:56 +0200

    add deletion of has_torch_package

--- src/graphnet/data/dataset/sqlite/__init__.py
+++ src/graphnet/data/dataset/sqlite/__init__.py
@@ -4,3 +4,5 @@ from graphnet.utilities.imports import has_torch_package
 if has_torch_package():
     from .sqlite_dataset import SQLiteDataset
     from .sqlite_dataset_perturbed import SQLiteDatasetPerturbed
+
+del has_torch_package
