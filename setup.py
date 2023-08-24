ce89ebdcdcd6c3cd4dc386152eec66750d42683f
AMHermansen <mail@andreashermansen.dk>
2023-08-24 09:40:18 +0200

    Added python 3.11 to CLASSIFIERS

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Environment :: CPU",
     "Environment :: GPU",
     "License :: OSI Approved :: Apache Software License",
