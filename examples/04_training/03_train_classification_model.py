761150f20e837505e16444c966fd806fa0b31d97
AMHermansen <mail@andreashermansen.dk>
2023-09-11 10:10:59 +0200

    Added check for wandb before logging to wandb

--- examples/04_training/03_train_classification_model.py
+++ examples/04_training/03_train_classification_model.py
@@ -106,7 +106,7 @@ def main(
     # Log configurations to W&B
     # NB: Only log to W&B on the rank-zero process in case of multi-GPU
     #     training.
-    if rank_zero_only == 0:
+    if wandb and rank_zero_only == 0:
         wandb_logger.experiment.config.update(config)
         wandb_logger.experiment.config.update(model_config.as_dict())
         wandb_logger.experiment.config.update(dataset_config.as_dict())
