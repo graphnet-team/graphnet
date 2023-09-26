43d357bbd6be438f0fd39fb654438202e48a6afc
Rasmus Oersoe <rahn@outlook.dk>
2023-09-26 12:48:45 +0200

    set run_name to __main__ for runpy

--- tests/examples/02_data/test_data_examples.py
+++ tests/examples/02_data/test_data_examples.py
@@ -8,26 +8,33 @@ EXAMPLE_PATH = os.path.join(GRAPHNET_ROOT_DIR, "examples/02_data")
 
 def test_01_read_dataset() -> None:
     """Test for 01_read_dataset."""
-    runpy.run_path(os.path.join(EXAMPLE_PATH, "01_read_dataset.py"))
+    runpy.run_path(
+        os.path.join(EXAMPLE_PATH, "01_read_dataset.py"), run_name="__main__"
+    )
 
 
 def test_02_plot_feature_distribution() -> None:
     """Test for 02_plot_feature_distribution."""
     runpy.run_path(
-        os.path.join(EXAMPLE_PATH, "02_plot_feature_distributions.py")
+        os.path.join(EXAMPLE_PATH, "02_plot_feature_distributions.py"),
+        run_name="__main__",
     )
 
 
 def test_03_convert_parquet_to_sqlite() -> None:
     """Test for 03_convert_parquet_to_sqlite."""
     runpy.run_path(
-        os.path.join(EXAMPLE_PATH, "03_convert_parquet_to_sqlite.py")
+        os.path.join(EXAMPLE_PATH, "03_convert_parquet_to_sqlite.py"),
+        run_name="__main__",
     )
 
 
 def test_04_ensemble_dataset() -> None:
     """Test for 04_ensemble_dataset."""
-    runpy.run_path(os.path.join(EXAMPLE_PATH, "04_ensemble_dataset.py"))
+    runpy.run_path(
+        os.path.join(EXAMPLE_PATH, "04_ensemble_dataset.py"),
+        run_name="__main__",
+    )
 
 
 if __name__ == "__main__":
--- tests/examples/04_training/test_training_examples.py
+++ tests/examples/04_training/test_training_examples.py
@@ -8,25 +8,32 @@ EXAMPLE_PATH = os.path.join(GRAPHNET_ROOT_DIR, "examples/04_training")
 
 def test_01_train_dynedge() -> None:
     """Test for 01_train_dynedge."""
-    runpy.run_path(os.path.join(EXAMPLE_PATH, "01_train_dynedge.py"))
+    runpy.run_path(
+        os.path.join(EXAMPLE_PATH, "01_train_dynedge.py"), run_name="__main__"
+    )
 
 
 def test_02_train_tito_model() -> None:
     """Test for 02_train_tito_model."""
-    runpy.run_path(os.path.join(EXAMPLE_PATH, "02_train_tito_model.py"))
+    runpy.run_path(
+        os.path.join(EXAMPLE_PATH, "02_train_tito_model.py"),
+        run_name="__main__",
+    )
 
 
 def test_03_train_dynedge_from_config() -> None:
     """Test for 03_train_dynedge_from_config."""
     runpy.run_path(
-        os.path.join(EXAMPLE_PATH, "03_train_dynedge_from_config.py")
+        os.path.join(EXAMPLE_PATH, "03_train_dynedge_from_config.py"),
+        run_name="__main__",
     )
 
 
 def test_04_train_multiclassifier_from_configs() -> None:
     """Test for 04_train_multiclassifier_from_configs."""
     runpy.run_path(
-        os.path.join(EXAMPLE_PATH, "04_train_multiclassifier_from_configs.py")
+        os.path.join(EXAMPLE_PATH, "04_train_multiclassifier_from_configs.py"),
+        run_name="__main__",
     )
 
 
