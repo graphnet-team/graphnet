7b3e251057dab9ca5b15681ded035caf15306247
Rasmus Oersoe <rahn@outlook.dk>
2023-10-19 15:14:33 +0200

    add ORCA150

--- src/graphnet/models/detector/prometheus.py
+++ src/graphnet/models/detector/prometheus.py
@@ -27,3 +27,33 @@ class Prometheus(Detector):
 
     def _t(self, x: torch.tensor) -> torch.tensor:
         return ((x / 1.05e04) - 1.0) * 20.0
+
+
+class ORCA150(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = (
+        "/home/iwsatlas1/oersoe/phd/tmp/geometry_tables/geometry_table.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 100
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return (x + 350) / 100
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
