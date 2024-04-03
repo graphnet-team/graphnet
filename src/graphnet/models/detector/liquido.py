35e7664f0f7a305dfc549eb01b51c39b5fe0b9f7
Rasmus Oersoe <rahn@outlook.dk>
2024-04-03 11:18:07 +0200

    add detector class for liquid o

---
+++ src/graphnet/models/detector/liquido.py
@@ -0,0 +1,35 @@
+"""LiquidO-specific `Detector` class(es)."""
+
+from typing import Dict, Callable
+import torch
+import os
+
+from graphnet.models.detector.detector import Detector
+from graphnet.constants import LIQUIDO_GEOMETRY_TABLE_DIR
+
+
+class LiquidO_v1(Detector):
+    """`Detector` class for LiquidO prototype."""
+
+    geometry_table_path = os.path.join(
+        LIQUIDO_GEOMETRY_TABLE_DIR, "liquiodo_v1.parquet"
+    )
+    xyz = ["sipm_x", "sipm_y", "sipm_z"]
+    string_id_column = "fiber_id"
+    sensor_id_column = "sipm_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sipm_x": self._sipm_xyz,
+            "sipm_y": self._sipm_xyz,
+            "sipm_z": self._sipm_xyz,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sipm_xyz_xyz(self, x: torch.tensor) -> torch.tensor:
+        return x / 1000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 500
