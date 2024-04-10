b245f0788f35e646e972ea85ff97e6245e82a936
Rasmus Oersoe <rahn@outlook.dk>
2024-04-10 09:19:42 +0200

    add all public detector classes from Prometheus

--- src/graphnet/models/detector/prometheus.py
+++ src/graphnet/models/detector/prometheus.py
@@ -8,7 +8,7 @@ from graphnet.models.detector.detector import Detector
 from graphnet.constants import PROMETHEUS_GEOMETRY_TABLE_DIR
 
 
-class ORCA150(Detector):
+class ORCA150SuperDense(Detector):
     """`Detector` class for Prometheus prototype."""
 
     geometry_table_path = os.path.join(
@@ -38,5 +38,302 @@ class ORCA150(Detector):
         return x / 1.05e04
 
 
-class Prometheus(ORCA150):
-    """Reference to ORCA150."""
+class TRIDENT1211(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "trident.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 1900
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 3000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class IceCubeUpgrade7(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "icecube_upgrade.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 10
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 2000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class WaterDemo81(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "demo_water.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 500
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 2000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class BaikalGVD8(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "gvd.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 10
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 1000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class IceDemo81(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "demo_ice.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 500
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 3000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class ARCA115(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "arca.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 100
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 1000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class ORCA150(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "orca.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 10
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 100
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class IceCube86(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "icecube86.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 100
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 1000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class IceCubeDeepCore8(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "icecube_deepcore.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xy,
+            "sensor_pos_y": self._sensor_pos_xy,
+            "sensor_pos_z": self._sensor_pos_z,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 100
+
+    def _sensor_pos_z(self, x: torch.tensor) -> torch.tensor:
+        return x / 1000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class IceCubeGen2(Detector):
+    """`Detector` class for Prometheus prototype."""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "icecube_gen2.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xyz,
+            "sensor_pos_y": self._sensor_pos_xyz,
+            "sensor_pos_z": self._sensor_pos_xyz,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xyz(self, x: torch.tensor) -> torch.tensor:
+        return x / 1000
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
+
+class Prometheus(ORCA150SuperDense):
+    """Reference to ORCA150SuperDense."""
