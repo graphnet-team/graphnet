9e87bcaf1addc4591fba8e6b608b5443dcc095d8
Rasmus Oersoe <rahn@outlook.dk>
2024-05-01 10:34:27 +0200

    add P-ONE detector class for triangle geometry

--- src/graphnet/models/detector/prometheus.py
+++ src/graphnet/models/detector/prometheus.py
@@ -333,6 +333,33 @@ class IceCubeGen2(Detector):
 
     def _t(self, x: torch.tensor) -> torch.tensor:
         return x / 1.05e04
+    
+class PONETriangle(Detector):
+    """`Detector` class for Prometheus PONE Triangle"""
+
+    geometry_table_path = os.path.join(
+        PROMETHEUS_GEOMETRY_TABLE_DIR, "pone_triangle.parquet"
+    )
+    xyz = ["sensor_pos_x", "sensor_pos_y", "sensor_pos_z"]
+    string_id_column = "sensor_string_id"
+    sensor_id_column = "sensor_id"
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "sensor_pos_x": self._sensor_pos_xyz,
+            "sensor_pos_y": self._sensor_pos_xyz,
+            "sensor_pos_z": self._sensor_pos_xyz,
+            "t": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xyz(self, x: torch.tensor) -> torch.tensor:
+        return x / 100
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x / 1.05e04
+
 
 
 class Prometheus(ORCA150SuperDense):
