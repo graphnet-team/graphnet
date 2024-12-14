02d4a0d7be067eb44e2753e151e21f0f528d49b0
Philip Weigel <pweigel@mit.edu>
2024-12-14 17:00:50 -0500

    Remove duplicate pyg_softmax function

--- src/graphnet/models/components/layers.py
+++ src/graphnet/models/components/layers.py
@@ -597,29 +597,6 @@ class Block(LightningModule):
             x = x + self.drop_path(self.gamma_2 * self.mlp(self.norm2(x)))
         return x
 
-def pyg_softmax(src, index, num_nodes=None):
-    r"""Computes a sparsely evaluated softmax.
-    Given a value tensor :attr:`src`, this function first groups the values
-    along the first dimension based on the indices specified in :attr:`index`,
-    and then proceeds to compute the softmax individually for each group.
-
-    Args:
-        src (Tensor): The source tensor.
-        index (LongTensor): The indices of elements for applying the softmax.
-        num_nodes (int, optional): The number of nodes, *i.e.*
-            :obj:`max_val + 1` of :attr:`index`. (default: :obj:`None`)
-
-    :rtype: :class:`Tensor`
-    """
-
-    num_nodes = maybe_num_nodes(index, num_nodes)
-
-    out = src - scatter_max(src, index, dim=0, dim_size=num_nodes)[0][index]
-    out = out.exp()
-    out = out / (
-            scatter_add(out, index, dim=0, dim_size=num_nodes)[index] + 1e-16)
-
-    return out
 
 class MultiHeadAttentionLayerGritSparse(LightningModule):
     """
