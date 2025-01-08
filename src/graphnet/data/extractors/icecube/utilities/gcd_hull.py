532380b6374a745f28e50ead337289b1dd74bab0
askerosted@gmail.com <askerosted@gmail.com>
2025-01-08 11:46:02 +0900

    make GCD_hull pickleable

--- src/graphnet/data/extractors/icecube/utilities/gcd_hull.py
+++ src/graphnet/data/extractors/icecube/utilities/gcd_hull.py
@@ -22,6 +22,8 @@ class GCD_hull(ConvexHull):
         """Initialize the ConvexHull object from the GCD file."""
         # Member variable(s)
         # IceCube surface object
+        self.gcd_file = gcd_file
+        self.padding = padding
         self.surface = MuonGun.ExtrudedPolygon.from_file(
             gcd_file, padding=padding
         )
@@ -56,3 +58,20 @@ class GCD_hull(ConvexHull):
             (np.dot(eq[:-1], point) + eq[-1] <= tolerance)
             for eq in self.equations
         )
+
+    def __getstate__(self) -> dict:
+        """Return the state of the object for pickling."""
+        state = self.__dict__.copy()
+        # Remove the unpicklable entries.
+        if "surface" in state:
+            del state["surface"]
+        return state
+
+    def __setstate__(self, state: dict) -> None:
+        """Restore the state of the object from the unpickled state."""
+        self.__dict__.update(state)
+        # Recreate the surface object if necessary.
+        if "surface" not in self.__dict__:
+            self.surface = MuonGun.ExtrudedPolygon.from_file(
+                self.gcd_file, padding=self.padding
+            )
