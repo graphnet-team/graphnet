2b378b3f964eb8e3d6bb7168ad177947bab71f12
Severin Magel <severin.magel@tum.de>
2025-02-21 13:23:42 +0100

    fix imports for testing

---
+++ src/graphnet/models/graphs/edges/minkowski/__init__.py
@@ -0,0 +1,8 @@
+"""Deprecated imports.
+
+moved to graphnet.models.data_representation.graphs.edges.
+"""
+
+from graphnet.models.data_representation.graphs.edges.minkowski import (
+    compute_minkowski_distance_mat,
+)
