8c3483589ee83e1d37795b05a4bebadd0f097e50
niklasmei <mei-nik@gmx.de>
2025-07-15 11:46:58 +0200

    renamed file and brought up to date

---
+++ src/graphnet/models/gnn/pretraining_maskpred.py
@@ -0,0 +1,210 @@
+from typing import Set, Union, List, Type, Optional, Dict
+import os
+
+import torch
+from torch.optim.adam import Adam
+from torch.functional import Tensor
+
+from torch_geometric.nn.pool.select.topk import topk
+from torch_geometric.utils import mask_select
+from torch_geometric.data import Data
+
+from torch_scatter import scatter
+
+from graphnet.models import Model
+from graphnet.models.easy_model import EasySyntax
+from graphnet.models.task import IdentityTask
+
+from graphnet.training.loss_functions import MSELoss
+
+
+def dense_mse_loss(reco, orig, bv):
+    squared_errs = (reco - orig)**2
+    losses = torch.mean(scatter(src=squared_errs, index=bv, reduce='mean', dim=0), dim=0)
+
+    return losses.view(-1,1)
+
+def neg_cosine_loss(reco, orig, bv):
+    reco_norm = torch.nn.functional.normalize(reco, dim=1)
+    orig_norm = torch.nn.functional.normalize(orig, dim=1)
+    cos = -(reco_norm*orig_norm).sum(dim=1)
+    losses = scatter(src=cos, index=bv, reduce='mean', dim=0)
+
+    return losses.view(-1,1)
+
+class standard_maskpred_net(Model):
+    def __init__(self,
+                 in_dim: int,
+                 hidden_dim: int = 1000,
+                 out_dim: int = 5,
+                 nb_linear: int = 5,
+                 ):
+        super().__init__()
+
+        self.activation = torch.nn.SELU()
+        
+        self.lin_net = torch.nn.ModuleList()
+        for i in range(nb_linear):
+            if i == 0:
+                self.lin_net.append(torch.nn.Linear(in_dim,hidden_dim))
+            else:
+                self.lin_net.append(torch.nn.Linear(hidden_dim,hidden_dim))
+
+        self.final_proj = torch.nn.Linear(hidden_dim, out_dim)
+        
+
+    def forward(self, data:Union[Data, Tensor]):
+        if isinstance(data, Data):
+            x_hat = data.x
+        else:
+            x_hat = data
+        x_hat = self.lin_net[0](x_hat)
+        x_hat = self.activation(x_hat)
+        for i in range(1,len(self.lin_net)):
+            x_hat = x_hat + self.lin_net[i](x_hat)
+            x_hat = self.activation(x_hat)
+
+        x_hat = self.final_proj(x_hat)
+        
+        return x_hat
+
+class mask_pred_augment(Model):
+    def __init__(self, 
+                 masked_ratio: float = 0.25,
+                 masked_feat: List[int] = [0,1,2,3,4],
+                 hlc_pos: int = None,):
+        super().__init__()
+        self.ratio = masked_ratio
+        self.hlc_pos = hlc_pos
+        self.masked_feat = masked_feat
+
+    def forward(self, data: Data):
+        auged = data.clone()
+
+        rand_score = torch.rand_like(data.batch.to(dtype=torch.bfloat16))
+        if self.hlc_pos is not None:
+            rand_score = rand_score + auged.x[:,self.hlc_pos].view(1,-1)
+            rand_score = rand_score.view(-1)
+
+        ind = topk(x=rand_score, ratio=self.ratio, batch=data.batch)
+
+        mask = torch.ones_like(data.batch.to(dtype=torch.bfloat16))
+        mask[ind] = 0
+
+        target = mask_select(src=auged.x, dim=0, mask=~mask.bool())[:,self.masked_feat]
+        auged.x[:,self.masked_feat] = auged.x[:,self.masked_feat]*mask.view(-1,1)
+
+        return auged, target
+
+class mask_pred_frame(EasySyntax):
+    def __init__(self,
+                 encoder: Model,
+                 encoder_out_dim: int = None,
+                 masked_ratio: float = 0.25,
+                 masked_feat: List[int] = [0,1,2,3,4],
+                 hlc_pos: int = None,
+                 mask_pred_net: Model = None,
+                 default_hidden_dim: int = 1000, 
+                 default_nb_linear: int = 5,
+                 final_loss: str = 'cosine',
+                 optimizer_class: Type[torch.optim.Optimizer] = Adam,
+                 optimizer_kwargs: Optional[Dict] = None,
+                 scheduler_class: Optional[type] = None,
+                 scheduler_kwargs: Optional[Dict] = None,
+                 scheduler_config: Optional[Dict] = None,) -> None:
+        
+        #just because I need to specify a task
+        task = IdentityTask(nb_outputs=1, target_labels=['skip'], hidden_size=2, loss_function=MSELoss())
+
+        super().__init__(
+            tasks=task,
+            optimizer_class=optimizer_class,
+            optimizer_kwargs=optimizer_kwargs,
+            scheduler_class=scheduler_class,
+            scheduler_kwargs=scheduler_kwargs,
+            scheduler_config=scheduler_config,
+        )
+        self.backbone = encoder
+
+        self.ratio = masked_ratio
+
+        self.augment = mask_pred_augment(masked_ratio=masked_ratio,
+                                         masked_feat=masked_feat,
+                                         hlc_pos=hlc_pos)
+
+        if encoder_out_dim is None:
+            assert encoder.nb_outputs > 0, 'make sure to either specify \"encoder_out_dim\" or have a \".nb_outputs\" in your encoder'
+            lat_dim = encoder.nb_outputs
+        else:
+            lat_dim = encoder_out_dim
+
+        if mask_pred_net is None:
+            print('no custom net for mask prediction specified; using a standard net')
+            self.rep = standard_maskpred_net(in_dim=lat_dim,
+                                             hidden_dim=default_hidden_dim,
+                                             out_dim=len(masked_feat),
+                                             nb_linear=default_nb_linear)
+        else:
+            assert mask_pred_net.nb_outputs == len(masked_feat), f'make sure that your \"mask_pred_net\" has number of output feats equal to nb of masked feats ({len(masked_feat)})'
+            self.rep = mask_pred_net
+
+        self.scorer = standard_maskpred_net(in_dim=len(masked_feat),
+                                            hidden_dim=default_hidden_dim,
+                                            out_dim=1,
+                                            nb_linear=default_nb_linear)
+        
+        assert final_loss in ['cosine', 'mse'], f'can only choose from {['cosine', 'mse']} for loss function'
+        if final_loss == 'cosine':
+            self.loss_func = neg_cosine_loss
+        elif final_loss == 'mse':
+            self.loss_func = dense_mse_loss
+            
+    def forward(self, data: Union[Data, List[Data]]):
+        if not isinstance(data, Data):
+            data = data[0]
+
+        aug, target = self.augment(data)
+
+        data_hat = self.backbone(aug)
+
+        rep = self.rep(data_hat)
+        score = self.scorer(rep)
+
+        ind = topk(x=score, ratio=self.ratio, batch=data.batch)
+
+        nodes = rep[ind]*torch.sigmoid(score[ind])
+
+        loss = self.loss_func(reco=nodes, orig=target, bv=data.batch[ind])
+
+        #loss is returned as a list to comply with the graphnet predict functionality
+        return [loss]
+
+    def validate_tasks(self) -> None:
+        accepted_tasks = IdentityTask
+        for task in self._tasks:
+            assert isinstance(task, accepted_tasks)
+
+    def shared_step(self, batch: List[Data], batch_idx: int):
+        loss = self(batch)
+        if isinstance(loss, list):
+            assert len(loss) == 1
+            loss = loss[0]
+        return torch.mean(loss, dim=0)
+    
+    def give_encoder_model(self):
+        #function to return the encoder model
+        #as a way to transport the pretrained encoder
+        #into another learning context or saving the parameters manually
+        return self.backbone
+    
+    def save_pretrained_model(self, save_path):
+        model = self.backbone
+
+        run_name = 'pretrained_model'
+
+        save_path = os.path.join(save_path, run_name)
+        print('saving to', save_path)
+        os.makedirs(save_path, exist_ok=True)
+
+        model.save_state_dict(f"{save_path}/state_dict.pth")
+        model.save_config(f"{save_path}/model_config.yml")
