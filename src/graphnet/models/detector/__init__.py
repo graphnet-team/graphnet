50d13dc9e375eb99d9daaf4d91247b12866e9872
Jarred Green <astrojarred@gmail.com>
2025-09-29 16:00:03 +0200

    Add MAGIC Telescope detector class Addresses #836

--- src/graphnet/models/detector/__init__.py
+++ src/graphnet/models/detector/__init__.py
@@ -4,3 +4,4 @@ from .icecube import IceCube86, IceCubeDeepCore, IceCubeUpgrade
 from .detector import Detector
 from .liquido import LiquidO_v1
 from .prometheus import ORCA150
+from .magic import MAGIC
---
+++ src/graphnet/models/detector/magic.py
@@ -0,0 +1,46 @@
+"""MAGIC-specific `Detector` class(es)."""
+
+
+from typing import Dict, Callable
+import torch
+import os
+
+from graphnet.models.detector.detector import Detector
+from graphnet.constants import MAGIC_GEOMETRY_TABLE_DIR
+
+class MAGIC(Detector):
+    """`Detector` class for MAGIC telescopes."""
+
+    geometry_table_path = os.path.join(MAGIC_GEOMETRY_TABLE_DIR, "magic.parquet")
+
+    # By default, treat the telescope ID as a spatial-like z-coordinate
+    xyz = ["x_cam", "y_cam", "tel_id"]
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension of input data."""
+        feature_map = {
+            "x_cam": self._xy,
+            "y_cam": self._xy,
+            "tel_id": self._identity,
+            "time": self._time,
+            "charge": self._charge,
+            "telescope_phi": self._identity,
+            "telescope_theta": self._identity,
+        }
+        return feature_map
+
+    def _xy(self, x: torch.tensor) -> torch.tensor:
+        return x / 28.5
+
+    def _time(self, x: torch.tensor) -> torch.tensor:
+        """Simple time scaling based on average time of arrival."""
+        t_min = -30
+        t_max = 60
+        # factor = 1.0 instead of 0.11 - this is the key change!
+        return (x - t_min) / (t_max - t_min)
+    
+    def _charge(self, x: torch.tensor) -> torch.tensor:
+        """Add a small epsilon to avoid log(0)"""
+        epsilon = 1e-6
+        return torch.log10(x + epsilon)
+
