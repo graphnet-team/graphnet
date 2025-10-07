f92a13f26f946e91de270e06536519459d653475
niklasmei <mei-nik@gmx.de>
2025-10-07 14:23:32 +0200

    added in the charge prediction functionality and learend masking values

--- src/graphnet/models/gnn/pretraining_maskpred.py
+++ src/graphnet/models/gnn/pretraining_maskpred.py
@@ -1,4 +1,4 @@
-from typing import Set, Union, List, Type, Optional, Dict
+from typing import Tuple, Union, List, Type, Optional, Dict
 import os
 
 import torch
@@ -38,7 +38,7 @@ class standard_maskpred_net(Model):
                  hidden_dim: int = 1000,
                  out_dim: int = 5,
                  nb_linear: int = 5,
-                 ):
+                 ) -> None:
         super().__init__()
 
         self.activation = torch.nn.SELU()
@@ -53,7 +53,7 @@ class standard_maskpred_net(Model):
         self.final_proj = torch.nn.Linear(hidden_dim, out_dim)
         
 
-    def forward(self, data:Union[Data, Tensor]):
+    def forward(self, data:Union[Data, Tensor]) -> Tensor:
         if isinstance(data, Data):
             x_hat = data.x
         else:
@@ -72,13 +72,21 @@ class mask_pred_augment(Model):
     def __init__(self, 
                  masked_ratio: float = 0.25,
                  masked_feat: List[int] = [0,1,2,3,4],
-                 hlc_pos: int = None,):
+                 learned_masking_value: bool = True,
+                 hlc_pos: int = None,
+                 ) -> None:
+        
         super().__init__()
         self.ratio = masked_ratio
         self.hlc_pos = hlc_pos
         self.masked_feat = masked_feat
+        self.learned_value = learned_masking_value
 
-    def forward(self, data: Data):
+        if self.learned_value:
+            print('warning: can currently only mask adjacent features, e.g. only (x,y,z) or only (t,q) but not e.g. (x,t,q)')
+            self.values = torch.nn.Parameter(torch.randn(1,len(self.masked_feat)))
+
+    def forward(self, data: Data) -> Tuple[Union[Data,Tensor]]:
         auged = data.clone()
 
         rand_score = torch.rand_like(data.batch.to(dtype=torch.bfloat16))
@@ -92,21 +100,29 @@ class mask_pred_augment(Model):
         mask[ind] = 0
 
         target = mask_select(src=auged.x, dim=0, mask=~mask.bool())[:,self.masked_feat]
-        auged.x[:,self.masked_feat] = auged.x[:,self.masked_feat]*mask.view(-1,1)
+        if not self.learned_value:
+            auged.x[:,self.masked_feat] = auged.x[:,self.masked_feat]*mask.view(-1,1)
+        else:
+            auged.x[ind,self.masked_feat[0]:self.masked_feat[-1]+1] = self.values
 
-        return auged, target
-
+        #returned mask is zero at the target position and 1 else
+        return auged, target, mask
+    
 class mask_pred_frame(EasySyntax):
     def __init__(self,
                  encoder: Model,
                  encoder_out_dim: int = None,
                  masked_ratio: float = 0.25,
                  masked_feat: List[int] = [0,1,2,3,4],
+                 learned_masking_value: bool = True,
                  hlc_pos: int = None,
                  mask_pred_net: Model = None,
                  default_hidden_dim: int = 1000, 
                  default_nb_linear: int = 5,
-                 final_loss: str = 'cosine',
+                 final_loss: str = 'mse',
+                 add_charge_pred: bool = False,
+                 need_charge_rep: bool = False,
+                 custom_charge_target: Tensor = None,
                  optimizer_class: Type[torch.optim.Optimizer] = Adam,
                  optimizer_kwargs: Optional[Dict] = None,
                  scheduler_class: Optional[type] = None,
@@ -130,7 +146,9 @@ class mask_pred_frame(EasySyntax):
 
         self.augment = mask_pred_augment(masked_ratio=masked_ratio,
                                          masked_feat=masked_feat,
-                                         hlc_pos=hlc_pos)
+                                         learned_masking_value=learned_masking_value,
+                                         hlc_pos=hlc_pos
+                                         )
 
         if encoder_out_dim is None:
             assert encoder.nb_outputs > 0, 'make sure to either specify \"encoder_out_dim\" or have a \".nb_outputs\" in your encoder'
@@ -147,34 +165,55 @@ class mask_pred_frame(EasySyntax):
         else:
             assert mask_pred_net.nb_outputs == len(masked_feat), f'make sure that your \"mask_pred_net\" has number of output feats equal to nb of masked feats ({len(masked_feat)})'
             self.rep = mask_pred_net
-
-        self.scorer = standard_maskpred_net(in_dim=len(masked_feat),
-                                            hidden_dim=default_hidden_dim,
-                                            out_dim=1,
-                                            nb_linear=default_nb_linear)
         
         assert final_loss in ['cosine', 'mse'], f'can only choose from {['cosine', 'mse']} for loss function'
         if final_loss == 'cosine':
             self.loss_func = neg_cosine_loss
         elif final_loss == 'mse':
             self.loss_func = dense_mse_loss
+
+        self.custom_charge_target = custom_charge_target
+        self.add_charge_pred = add_charge_pred
+        self.need_charge_rep = need_charge_rep
+        if add_charge_pred:
+            self.charge_net = torch.nn.Linear(lat_dim, 1)
+            if need_charge_rep:
+                self.lin_layer_scatter = torch.nn.Linear(lat_dim, lat_dim)
+
             
-    def forward(self, data: Union[Data, List[Data]]):
+    def forward(self, data: Union[Data, List[Data]]) -> List[Tensor]:
         if not isinstance(data, Data):
             data = data[0]
 
-        aug, target = self.augment(data)
+        aug, target, mask = self.augment(data)
 
-        data_hat = self.backbone(aug)
+        if not self.need_charge_rep:
+            data_hat, cls_tensor = self.backbone(aug)
+        else:
+            data_hat = self.backbone(aug)
+            reduce_list = ['sum', 'mean', 'max', 'min']
+            cls_tensor = scatter(src=data_hat, index=data.batch, reduce=reduce_list[0], dim=0)
+            for i in range(1,len(reduce_list)):
+                cls_tensor = cls_tensor + scatter(src=data_hat, index=data.batch, reduce=reduce_list[i], dim=0)
+            cls_tensor = self.lin_layer_scatter(cls_tensor)
+
+        assert len(data_hat.shape) == 2, 'dense data representation [n_pulses, lat_dim] is required for the processed tensor as an artifact'
 
         rep = self.rep(data_hat)
-        score = self.scorer(rep)
 
-        ind = topk(x=score, ratio=self.ratio, batch=data.batch)
+        nodes = rep[~mask.bool()]
+        btch = data.batch[~mask.bool()]
 
-        nodes = rep[ind]*torch.sigmoid(score[ind])
+        loss = self.loss_func(reco=nodes, orig=target, bv=btch)
 
-        loss = self.loss_func(reco=nodes, orig=target, bv=data.batch[ind])
+        if self.add_charge_pred:
+            if self.custom_charge_target is None:
+                charge_tensor = torch.pow(10, data.x[:,4]).view(-1,1)
+                charge_sums = torch.log10(scatter(src=charge_tensor, index=data.batch, dim = 0, reduce='sum'))
+            else:
+                charge_sums = self.custom_charge_target
+            pred_charge = self.charge_net(cls_tensor)
+            loss = loss + (charge_sums - pred_charge)**2
 
         #loss is returned as a list to comply with the graphnet predict functionality
         return [loss]
@@ -184,20 +223,20 @@ class mask_pred_frame(EasySyntax):
         for task in self._tasks:
             assert isinstance(task, accepted_tasks)
 
-    def shared_step(self, batch: List[Data], batch_idx: int):
+    def shared_step(self, batch: List[Data], batch_idx: int) -> Tensor:
         loss = self(batch)
         if isinstance(loss, list):
             assert len(loss) == 1
             loss = loss[0]
         return torch.mean(loss, dim=0)
     
-    def give_encoder_model(self):
+    def give_encoder_model(self) -> Model:
         #function to return the encoder model
         #as a way to transport the pretrained encoder
         #into another learning context or saving the parameters manually
         return self.backbone
     
-    def save_pretrained_model(self, save_path):
+    def save_pretrained_model(self, save_path) -> None:
         model = self.backbone
 
         run_name = 'pretrained_model'
